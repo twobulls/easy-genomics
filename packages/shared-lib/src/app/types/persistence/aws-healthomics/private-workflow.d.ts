bebf93e9201ac5d91d513e716fc99deb03e78759
Zig Tan <zig.tan@twobulls.com>
2024-02-12 13:24:35 +0800

    feat(EG-319): add shared-lib persistence/aws-healthomics/private-workflow.d.ts

---
+++ packages/shared-lib/src/app/types/persistence/aws-healthomics/private-workflow.d.ts
@@ -0,0 +1,25 @@
+/**
+ * The following PrivateWorkflow model represents the data stored in the
+ * healthomics-private-workflow-table.
+ *
+ * {
+ *   Url: <string>,
+ *   Version: <string>,
+ *   Status: <string>,
+ *   EfsVolumeUri: <string>,
+ *   CreatedAt?: <string>,
+ *   CreatedBy?: <string>,
+ *   ModifiedAt?: <string>,
+ *   ModifiedBy?: <string>,
+ * }
+ */
+import { BaseAttributes } from '../base-entity';
+
+export interface PrivateWorkflow extends BaseAttributes {
+  Url: string; // DynamoDB Partition Key (String)
+  Version: string; // DynamoDB Sort Key (String)
+  Status: WorkflowStatus;
+  EfsVolumeUri?: string;
+}
+
+export type WorkflowStatus = 'New' | 'Cloned' | 'Branched' | 'Inspected' | 'Built' | 'Published' | 'Active' | 'Inactive' | null;

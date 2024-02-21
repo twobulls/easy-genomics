8905f0c228a7ec187e7b32afdb268fa4e1a379a1
Zig Tan <zig.tan@twobulls.com>
2024-02-21 10:58:08 +0800

    feat: update aws-healthomics-nested-stack to invoke simplified LambdaConstruct

--- packages/back-end/src/infra/stacks/aws-healthomics-nested-stack.ts
+++ packages/back-end/src/infra/stacks/aws-healthomics-nested-stack.ts
@@ -3,6 +3,7 @@ import { NestedStack } from 'aws-cdk-lib';
 import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 import { DynamoConstruct } from '../constructs/dynamodb-construct';
+import { LambdaConstruct } from '../constructs/lambda-construct';
 import { AwsHealthOmicsNestedStackProps } from '../types/back-end-stack';
 
 export class AwsHealthOmicsNestedStack extends NestedStack {
@@ -18,7 +19,19 @@ export class AwsHealthOmicsNestedStack extends NestedStack {
   }
 
   // AWS HealthOmics specific REST API endpoints / Lambda Functions
-  private setupRestApiEndpoints = () => {};
+  private setupRestApiEndpoints = () => {
+    new LambdaConstruct(this, `${this.props.constructNamespace}-lambda`, {
+      ...this.props,
+      lambdaFunctionsDir: 'src/app/controllers/aws-healthomics',
+      lambdaFunctionsNamespace: `${this.props.constructNamespace}-lambda`,
+      lambdaFunctionsResources: {}, // Used for setting specific resources for a given Lambda function (e.g. environment settings, trigger events)
+      lambdaTimeoutInSeconds: this.props.lambdaTimeoutInSeconds,
+      environment: {
+        ENV_NAME: this.props.envName,
+        AWS_ACCOUNT_ID: this.props.env.account!,
+      },
+    });
+  };
 
   // AWS HealthOmics specific DynamoDB tables
   private setupDynamoDBTables = () => {

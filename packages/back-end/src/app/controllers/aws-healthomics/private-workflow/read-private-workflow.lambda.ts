9aee4ad7c1a638d46099260dfb4015b84b0114bd
Zig Tan <zig.tan@twobulls.com>
2024-02-23 11:42:23 +0800

    feat: add read-private-workflow.lambda.ts

---
+++ packages/back-end/src/app/controllers/aws-healthomics/private-workflow/read-private-workflow.lambda.ts
@@ -0,0 +1,34 @@
+import { PrivateWorkflow } from '@easy-genomics/shared-lib/src/app/types/persistence/aws-healthomics/private-workflow';
+import { buildResponse } from '@easy-genomics/shared-lib/src/app/utils/common';
+import { APIGatewayProxyResult, APIGatewayProxyWithCognitoAuthorizerEvent, Handler } from 'aws-lambda';
+import { PrivateWorkflowService } from '../../../services/aws-healthomics/private-workflow-service';
+
+const privateWorkflowService = new PrivateWorkflowService();
+
+export const handler: Handler = async (
+  event: APIGatewayProxyWithCognitoAuthorizerEvent,
+): Promise<APIGatewayProxyResult> => {
+  console.log('EVENT: \n' + JSON.stringify(event, null, 2));
+  try {
+    // Get Path Parameter
+    const id: string = event.pathParameters?.id || '';
+    if (id === '') throw new Error('Required id is missing');
+
+    // Lookup by GSI Id for convenience
+    const existing: PrivateWorkflow = await privateWorkflowService.find(id);
+    return buildResponse(200, JSON.stringify(existing), event);
+  } catch (err: any) {
+    console.error(err);
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        Error: getErrorMessage(err),
+      }),
+    };
+  }
+};
+
+// Used for customising error messages by exception types
+function getErrorMessage(err: any) {
+  return err.message;
+};

9b81b70ddd732e08bc71a77465052e5d94572cd5
Zig Tan <zig.tan@twobulls.com>
2024-02-23 11:42:47 +0800

    feat: add request-private-workflow.lambda.ts

---
+++ packages/back-end/src/app/controllers/aws-healthomics/private-workflow/request-private-workflow.lambda.ts
@@ -0,0 +1,36 @@
+import { PrivateWorkflow } from '@easy-genomics/shared-lib/src/app/types/persistence/aws-healthomics/private-workflow';
+import { buildResponse } from '@easy-genomics/shared-lib/src/app/utils/common';
+import { APIGatewayProxyResult, APIGatewayProxyWithCognitoAuthorizerEvent, Handler } from 'aws-lambda';
+import { PrivateWorkflowService } from '../../../services/aws-healthomics/private-workflow-service';
+
+const privateWorkflowService = new PrivateWorkflowService();
+
+export const handler: Handler = async (
+  event: APIGatewayProxyWithCognitoAuthorizerEvent,
+): Promise<APIGatewayProxyResult> => {
+  console.log('EVENT: \n' + JSON.stringify(event, null, 2));
+  try {
+    // Post Request Body
+    const request: PrivateWorkflow = (
+      event.isBase64Encoded ? JSON.parse(atob(event.body!)) : JSON.parse(event.body!)
+    );
+    if (request.Url === '') throw new Error('Required Url is missing');
+    if (request.Version === '') throw new Error('Required Version is missing');
+
+    const response: PrivateWorkflow = await privateWorkflowService.get(request.Url, request.Version);
+    return buildResponse(200, JSON.stringify(response), event);
+  } catch (err: any) {
+    console.error(err);
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        Error: getErrorMessage(err),
+      }),
+    };
+  }
+};
+
+// Used for customising error messages by exception types
+function getErrorMessage(err: any) {
+  return err.message;
+};

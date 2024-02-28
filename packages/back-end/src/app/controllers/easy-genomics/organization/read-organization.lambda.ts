49179a01f7415b338d8d6bc000297197dc8b39de
Zig Tan <zig.tan@twobulls.com>
2024-02-28 12:17:43 +0800

    feat: add read-organization lambda

---
+++ packages/back-end/src/app/controllers/easy-genomics/organization/read-organization.lambda.ts
@@ -0,0 +1,33 @@
+import { Organization } from '@easy-genomics/shared-lib/src/app/types/persistence/easy-genomics/organization';
+import { buildResponse } from '@easy-genomics/shared-lib/src/app/utils/common';
+import { APIGatewayProxyResult, APIGatewayProxyWithCognitoAuthorizerEvent, Handler } from 'aws-lambda';
+import { OrganizationService } from '../../../services/easy-genomics/organization-service';
+
+const organizationService = new OrganizationService();
+
+export const handler: Handler = async (
+  event: APIGatewayProxyWithCognitoAuthorizerEvent,
+): Promise<APIGatewayProxyResult> => {
+  console.log('EVENT: \n' + JSON.stringify(event, null, 2));
+  try {
+    // Get Path Parameter
+    const id: string = event.pathParameters?.id || '';
+    if (id === '') throw new Error('Required id is missing');
+
+    const existing: Organization = await organizationService.get(id);
+    return buildResponse(200, JSON.stringify(existing), event);
+  } catch (err: any) {
+    console.error(err);
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        Error: getErrorMessage(err),
+      }),
+    };
+  }
+};
+
+// Used for customising error messages by exception types
+function getErrorMessage(err: any) {
+  return err.message;
+};

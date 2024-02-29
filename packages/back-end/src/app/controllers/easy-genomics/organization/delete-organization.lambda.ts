9934cd6dd184fbd1093c01369ddd7c2b85474f53
Zig Tan <zig.tan@twobulls.com>
2024-02-29 08:09:35 +0800

    feat: add delete-organization lambda

---
+++ packages/back-end/src/app/controllers/easy-genomics/organization/delete-organization.lambda.ts
@@ -0,0 +1,37 @@
+import { Organization } from '@easy-genomics/shared-lib/src/app/types/persistence/easy-genomics/organization';
+import { buildResponse } from '@easy-genomics/shared-lib/src/app/utils/common';
+import { APIGatewayProxyResult, APIGatewayProxyWithCognitoAuthorizerEvent, Handler } from 'aws-lambda';
+import { OrganizationService } from '../../../services/easy-genomics/organization-service';
+
+const organizationService = new OrganizationService();
+
+export const handler: Handler = async (
+  event: APIGatewayProxyWithCognitoAuthorizerEvent,
+): Promise<APIGatewayProxyResult> => {
+  console.log('EVENT: \n' + JSON.stringify(event, null, 2));
+  try {
+    // Get Path Parameter
+    const id: string = event.pathParameters?.id || '';
+    if (id === '') throw new Error('Required id is missing');
+
+    // Lookup by OrganizationId to confirm existence before deletion
+    const existing: Organization = await organizationService.get(id);
+    const isDeleted: boolean = await organizationService.delete(existing.OrganizationId, existing.Name);
+    return buildResponse(200, JSON.stringify({
+      deleted: isDeleted,
+    }), event);
+  } catch (err: any) {
+    console.error(err);
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        Error: getErrorMessage(err),
+      }),
+    };
+  }
+};
+
+// Used for customising error messages by exception types
+function getErrorMessage(err: any) {
+  return err.message;
+};

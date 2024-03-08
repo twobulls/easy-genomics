2dd37aaa572b462558c1efa75b05ebe208f5c1c7
Zig Tan <zig.tan@twobulls.com>
2024-03-08 13:41:58 +0800

    feat: add read-laboratory lambda

---
+++ packages/back-end/src/app/controllers/easy-genomics/laboratory/read-laboratory.lambda.ts
@@ -0,0 +1,34 @@
+import { Laboratory } from '@easy-genomics/shared-lib/src/app/types/persistence/easy-genomics/laboratory';
+import { buildResponse } from '@easy-genomics/shared-lib/src/app/utils/common';
+import { APIGatewayProxyResult, APIGatewayProxyWithCognitoAuthorizerEvent, Handler } from 'aws-lambda';
+import { LaboratoryService } from '../../../services/easy-genomics/laboratory-service';
+
+const laboratoryService = new LaboratoryService();
+
+export const handler: Handler = async (
+  event: APIGatewayProxyWithCognitoAuthorizerEvent,
+): Promise<APIGatewayProxyResult> => {
+  console.log('EVENT: \n' + JSON.stringify(event, null, 2));
+  try {
+    // Get Path Parameter
+    const id: string = event.pathParameters?.id || '';
+    if (id === '') throw new Error('Required id is missing');
+
+    // Lookup by GSI Id for convenience
+    const existing: Laboratory = await laboratoryService.query(id);
+    return buildResponse(200, JSON.stringify(existing), event);
+  } catch (err: any) {
+    console.error(err);
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        Error: getErrorMessage(err),
+      }),
+    };
+  }
+};
+
+// Used for customising error messages by exception types
+function getErrorMessage(err: any) {
+  return err.message;
+};

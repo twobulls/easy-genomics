92b76af8ec6026409b4f9cf5f753ee827770ff3a
Zig Tan <zig.tan@twobulls.com>
2024-03-18 09:31:00 +0800

    feat: add laboratory zod schema definitions to validate api messages & safe guard DynamoDB

---
+++ packages/shared-lib/src/app/schema/easy-genomics/laboratory.ts
@@ -0,0 +1,34 @@
+import { z } from 'zod';
+
+export const LaboratorySchema = z.object({
+  OrganizationId: z.string().uuid(),
+  LaboratoryId: z.string().uuid(),
+  Name: z.string(),
+  S3Bucket: z.string().optional(),
+  Status: z.enum(['Active', 'Inactive']),
+  AwsHealthOmicsEnabled: z.boolean().optional(),
+  NextFlowTowerEnabled: z.boolean().optional(),
+  CreatedAt: z.string().optional(),
+  CreatedBy: z.string().optional(),
+  ModifiedAt: z.string().optional(),
+  ModifiedBy: z.string().optional(),
+}).strict();
+
+export const CreateLaboratorySchema = z.object({
+  OrganizationId: z.string().uuid(),
+  Name: z.string(),
+  Status: z.enum(['Active', 'Inactive']),
+}).strict();
+
+export const RequestLaboratorySchema = z.object({
+  OrganizationId: z.string().uuid(),
+  LaboratoryId: z.string().uuid(),
+}).strict();
+
+export const UpdateLaboratorySchema = z.object({
+  Name: z.string(),
+  S3Bucket: z.string().optional(),
+  Status: z.enum(['Active', 'Inactive']),
+  AwsHealthOmicsEnabled: z.boolean().optional(),
+  NextFlowTowerEnabled: z.boolean().optional(),
+}).strict();

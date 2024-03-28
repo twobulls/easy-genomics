55fda162666f3564eb8b110a84c1c0ed37ac5b65
Zig Tan <zig.tan@twobulls.com>
2024-03-28 14:47:41 +0800

    feat: update user zod data validation schema to support User creation

--- packages/shared-lib/src/app/schema/easy-genomics/user.ts
+++ packages/shared-lib/src/app/schema/easy-genomics/user.ts
@@ -7,7 +7,7 @@ export const UserSchema = z.object({
   FirstName: z.string().optional(),
   LastName: z.string().optional(),
   PhoneNumber: z.string().optional(),
-  Status: z.enum(['Active', 'Inactive']),
+  Status: z.enum(['Active', 'Inactive', 'Invited']),
   CreatedAt: z.string().optional(),
   CreatedBy: z.string().optional(),
   ModifiedAt: z.string().optional(),
@@ -15,7 +15,6 @@ export const UserSchema = z.object({
 }).strict();
 
 export const CreateUserSchema = z.object({
-  UserId: z.string().uuid(),
   Email: z.string(),
   Title: z.string().optional(),
   FirstName: z.string().optional(),

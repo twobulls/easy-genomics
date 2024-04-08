a077d82044a3872557a9f54eef4e2b1d6c7c6e67
Simon Dib <simon.dib@twobulls.com>
2024-04-09 09:49:47 +1000

    chore: cleanup button template

--- packages/front-end/src/app/components/EGButton.vue
+++ packages/front-end/src/app/components/EGButton.vue
@@ -22,10 +22,7 @@
     :class="{
       'bg-primary hover:bg-primary-dark text-black text-white disabled:bg-gray-400 disabled:opacity-100':
         variant === 'primary',
-      'border-lifeline-dark-blue-200 hover:bg-lifeline-dark-blue-100 border border-solid bg-white  hover:text-white ':
-        variant === 'secondary',
-      ' hover:bg-lifeline-dark-blue-50 text-white': variant === 'ghost',
-      ' hover:bg-lifeline-dark-blue-50 text-white': variant === 'destructive',
+      '': variant === 'secondary',
       'font-semibold': bold,
     }"
   >

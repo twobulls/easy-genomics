89405f6749502ba294b558dc4ab8feda26033c9f
Simon Dib <simon.dib@twobulls.com>
2024-04-16 08:10:06 +1000

    feat: table loading states

--- packages/front-end/src/app/pages/organizations.vue
+++ packages/front-end/src/app/pages/organizations.vue
@@ -28,6 +28,30 @@
   const pageTo = computed(() => Math.min(page.value * pageCount.value, pageTotal.value));
 
   const sortedData = computed(() => {
+    // return [
+    //   {
+    //     'ModifiedBy': '768369ad-2fd5-4ec5-8572-5c164837936f',
+    //     'Country': 'Australia',
+    //     'AwsHealthOmicsEnabled': true,
+    //     'CreatedBy': '768369ad-2fd5-4ec5-8572-5c164837936f',
+    //     'Description': 'Marvin desc - DEPT AU Health Division',
+    //     'ModifiedAt': '2024-04-15T06:23:40.679Z',
+    //     'NextFlowTowerEnabled': false,
+    //     'CreatedAt': '2024-04-15T04:20:35.491Z',
+    //     'Name': 'DEPT-Health',
+    //     'OrganizationId': '371b0f3e-8a9d-4edb-97ea-5345c0464706',
+    //   },
+    //   {
+    //     'Country': 'Japan',
+    //     'AwsHealthOmicsEnabled': false,
+    //     'Description': "Marvin's test Lab - 1 $%^$%$$^",
+    //     'CreatedBy': '768369ad-2fd5-4ec5-8572-5c164837936f',
+    //     'NextFlowTowerEnabled': false,
+    //     'OrganizationId': '12ae8bac-c2b3-4312-bd38-88bf3c0e3f15',
+    //     'CreatedAt': '2024-04-15T07:40:57.088Z',
+    //     'Name': 'Test Org 1',
+    //   },
+    // ];
     return orgData.value
       .slice((page.value - 1) * pageCount.value, page.value * pageCount.value)
       .sort((a, b) => a.Name.localeCompare(b.Name));
@@ -54,7 +78,7 @@
     ],
   ];
 
-  useFetch(async () => {
+  onMounted(async () => {
     try {
       orgData.value = await $api.orgs.list(MOCK_ORG_ID);
       if (!orgData.value.length) {
@@ -82,28 +106,35 @@
     button-label="Create a new Organization"
   />
 
-  <template v-if="!loading && !hasNoData">
-    <UCard
-      class="rounded-2xl border-none shadow-none"
-      :ui="{
-        body: 'p-0',
-      }"
+  <UCard
+    class="rounded-2xl border-none shadow-none"
+    :ui="{
+      body: 'p-0',
+    }"
+  >
+    <UTable
+      class="LabsTable rounded-2xl"
+      :rows="sortedData"
+      :columns="columns"
+      :key="sortedData"
+      :loading="isLoading"
+      :loading-state="{ icon: '', label: '' }"
     >
-      <UTable class="LabsTable rounded-2xl" :rows="sortedData" :columns="columns">
-        <template #actions-data="{ row }">
-          <EGActionButton :items="actionItems(row)" />
-        </template>
-        <template #empty-state>&nbsp;</template>
-      </UTable>
-    </UCard>
+      <template #actions-data="{ row }">
+        <EGActionButton :items="actionItems(row)" />
+      </template>
+      <template #empty-state>&nbsp;</template>
+    </UTable>
+  </UCard>
 
-    <div class="text-muted flex h-16 flex-wrap items-center justify-between">
-      <div class="text-xs leading-5">Showing {{ pageFrom }} to {{ pageTo }} results</div>
-      <div class="flex justify-end px-3" v-if="pageTotal > pageCount">
-        <UPagination v-model="page" :page-count="10" :total="labData.length" />
-      </div>
+  <div class="text-muted flex h-16 flex-wrap items-center justify-between" v-if="!isLoading || hasNoData">
+    <div class="text-xs leading-5">Showing {{ pageFrom }} to {{ pageTo }} results</div>
+    <div class="flex justify-end px-3" v-if="pageTotal > pageCount">
+      <UPagination v-model="page" :page-count="10" :total="labData.length" />
     </div>
-  </template>
+  </div>
+
+  <!--  <template v-else>123</template>-->
 </template>
 
 <style>
@@ -120,11 +151,11 @@
       }
 
       tr {
-        height: 50px;
         background-color: #efefef;
 
         th:first-child {
           padding-left: 40px;
+          min-width: 400px;
         }
       }
     }
@@ -136,7 +167,6 @@
     }
 
     tbody tr td:nth-child(2) {
-      min-width: 500px;
       font-size: 12px;
       color: #818181;
     }

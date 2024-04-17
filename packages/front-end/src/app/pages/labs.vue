f8a121bdd17fd9baf440d0a5eeb14d8fbab112a4
Simon Dib <simon.dib@twobulls.com>
2024-04-17 16:44:21 +1000

    refactor: fix sorting + refactor table as component

--- packages/front-end/src/app/pages/labs.vue
+++ packages/front-end/src/app/pages/labs.vue
@@ -3,8 +3,6 @@
   const hasNoData = ref(false);
   const isLoading = ref(true);
   const labData = ref([]);
-  const page = ref(1);
-  const pageCount = ref(10);
   const { MOCK_ORG_ID } = useRuntimeConfig().public;
 
   const columns = [
@@ -22,12 +20,6 @@
       label: '',
     },
   ];
-
-  const sortedData = computed(() => {
-    return labData.value
-      .slice((page.value - 1) * pageCount.value, page.value * pageCount.value)
-      .sort((a, b) => a.Name.localeCompare(b.Name));
-  });
 
   const actionItems = (row: Array<{}>) => [
     [
@@ -50,20 +42,17 @@
     ],
   ];
 
-  const pageTotal = computed(() => labData.value.length);
-  const pageFrom = computed(() => (page.value - 1) * pageCount.value + 1);
-  const pageTo = computed(() => Math.min(page.value * pageCount.value, pageTotal.value));
-
-  onMounted(async () => {
+  onBeforeMount(async () => {
     try {
       labData.value = await $api.labs.list(MOCK_ORG_ID);
+
       if (!labData.value.length) {
         hasNoData.value = true;
       }
       isLoading.value = false;
     } catch (error) {
-      console.error(error);
       isLoading.value = false;
+      console.error(error);
       throw error;
     }
   });
@@ -82,73 +71,12 @@
     button-label="Create a new Lab"
   />
 
-  <UCard
+  <EGTable
     v-else
-    class="rounded-2xl border-none shadow-none"
-    :ui="{
-      body: 'p-0',
-    }"
-  >
-    <UTable
-      class="LabsTable rounded-2xl"
-      :rows="sortedData"
-      :columns="columns"
-      :key="sortedData"
-      :loading="isLoading"
-      :loading-state="{ icon: '', label: '' }"
-    >
-      <template #actions-data="{ row }">
-        <EGActionButton :items="actionItems(row)" />
-      </template>
-      <template #empty-state>&nbsp;</template>
-    </UTable>
-  </UCard>
-
-  <div class="text-muted flex h-16 flex-wrap items-center justify-between" v-if="!isLoading && !hasNoData">
-    <div class="text-xs leading-5">Showing {{ pageFrom }} to {{ pageTo }} results</div>
-    <div class="flex justify-end px-3" v-if="pageTotal > pageCount">
-      <UPagination v-model="page" :page-count="10" :total="labData.length" />
-    </div>
-  </div>
+    :table-data="labData"
+    :columns="columns"
+    :is-loading="isLoading"
+    :action-items="actionItems"
+    :show-pagination="!isLoading && !hasNoData"
+  />
 </template>
-
-<style>
-  .LabsTable {
-    font-size: 14px;
-    width: 100%;
-    table-layout: auto;
-
-    thead {
-      border-bottom: 1px solid #e5e5e5;
-
-      button {
-        color: black;
-      }
-
-      tr {
-        background-color: #efefef;
-
-        th:first-child {
-          padding-left: 40px;
-          width: 400px;
-        }
-      }
-    }
-
-    tbody tr td:nth-child(1) {
-      color: black;
-      font-weight: 600;
-      padding-left: 40px;
-    }
-
-    tbody tr td:nth-child(2) {
-      font-size: 12px;
-      color: #818181;
-    }
-
-    tbody tr td:last-child {
-      width: 50px;
-      padding-right: 40px;
-    }
-  }
-</style>
--- packages/front-end/src/app/pages/organizations.vue
+++ packages/front-end/src/app/pages/organizations.vue
@@ -2,10 +2,8 @@
   const { $api } = useNuxtApp();
   const hasNoData = ref(false);
   const isLoading = ref(true);
-  const MOCK_ORG_ID = '6a80b4fa-1cfe-4345-ae62-cdc58dbec69c';
   const orgData = ref([]);
-  const page = ref(1);
-  const pageCount = ref(10);
+  const { MOCK_ORG_ID } = useRuntimeConfig().public;
 
   const columns = [
     {
@@ -22,16 +20,6 @@
       label: '',
     },
   ];
-
-  const pageTotal = computed(() => orgData.value.length);
-  const pageFrom = computed(() => (page.value - 1) * pageCount.value + 1);
-  const pageTo = computed(() => Math.min(page.value * pageCount.value, pageTotal.value));
-
-  const sortedData = computed(() => {
-    return orgData.value
-      .slice((page.value - 1) * pageCount.value, page.value * pageCount.value)
-      .sort((a, b) => a.Name.localeCompare(b.Name));
-  });
 
   const actionItems = (row: Array<{}>) => [
     [
@@ -62,8 +50,8 @@
       }
       isLoading.value = false;
     } catch (error) {
-      console.error(error);
       isLoading.value = false;
+      console.error(error);
       throw error;
     }
   });
@@ -82,73 +70,12 @@
     button-label="Create a new Organization"
   />
 
-  <UCard
+  <EGTable
     v-else
-    class="rounded-2xl border-none shadow-none"
-    :ui="{
-      body: 'p-0',
-    }"
-  >
-    <UTable
-      class="LabsTable rounded-2xl"
-      :rows="sortedData"
-      :columns="columns"
-      :key="sortedData"
-      :loading="isLoading"
-      :loading-state="{ icon: '', label: '' }"
-    >
-      <template #actions-data="{ row }">
-        <EGActionButton :items="actionItems(row)" />
-      </template>
-      <template #empty-state>&nbsp;</template>
-    </UTable>
-  </UCard>
-
-  <div class="text-muted flex h-16 flex-wrap items-center justify-between" v-if="!isLoading && !hasNoData">
-    <div class="text-xs leading-5">Showing {{ pageFrom }} to {{ pageTo }} results</div>
-    <div class="flex justify-end px-3" v-if="pageTotal > pageCount">
-      <UPagination v-model="page" :page-count="10" :total="orgData.length" />
-    </div>
-  </div>
+    :table-data="orgData"
+    :columns="columns"
+    :is-loading="isLoading"
+    :action-items="actionItems"
+    :show-pagination="!isLoading && !hasNoData"
+  />
 </template>
-
-<style>
-  .LabsTable {
-    font-size: 14px;
-    width: 100%;
-    table-layout: auto;
-
-    thead {
-      border-bottom: 1px solid #e5e5e5;
-
-      button {
-        color: black;
-      }
-
-      tr {
-        background-color: #efefef;
-
-        th:first-child {
-          padding-left: 40px;
-          min-width: 400px;
-        }
-      }
-    }
-
-    tbody tr td:nth-child(1) {
-      color: black;
-      font-weight: 600;
-      padding-left: 40px;
-    }
-
-    tbody tr td:nth-child(2) {
-      font-size: 12px;
-      color: #818181;
-    }
-
-    tbody tr td:last-child {
-      width: 50px;
-      padding-right: 40px;
-    }
-  }
-</style>

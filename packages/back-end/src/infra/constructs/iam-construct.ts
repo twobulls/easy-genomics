b6ea1638cb4c152edd7164f6d3370a5306bf12e8
Zig Tan <zig.tan@twobulls.com>
2024-04-18 14:23:18 +0800

    fix(EG-387): add missing IAM policies for additional data lookups for add-laboratory-user API (#43)
    * fix(EG-387): add missing IAM policies for additional data lookups for add-laboratory-user API
    
    * fix(EG-387): add additional IAM policy resource for querying the laboratory-table by GSI

--- packages/back-end/src/infra/constructs/iam-construct.ts
+++ packages/back-end/src/infra/constructs/iam-construct.ts
@@ -370,6 +370,19 @@ export class IamConstruct extends Construct {
       '/easy-genomics/laboratory/user/add-laboratory-user',
       [
         new PolicyStatement({
+          resources: [
+            `arn:aws:dynamodb:${this.props.env.region!}:${this.props.env.account!}:table/${this.props.namePrefix}-laboratory-table`,
+            `arn:aws:dynamodb:${this.props.env.region!}:${this.props.env.account!}:table/${this.props.namePrefix}-laboratory-table/index/*`,
+          ],
+          actions: ['dynamodb:Query'],
+          effect: Effect.ALLOW,
+        }),
+        new PolicyStatement({
+          resources: [`arn:aws:dynamodb:${this.props.env.region!}:${this.props.env.account!}:table/${this.props.namePrefix}-user-table`],
+          actions: ['dynamodb:GetItem'],
+          effect: Effect.ALLOW,
+        }),
+        new PolicyStatement({
           resources: [`arn:aws:dynamodb:${this.props.env.region!}:${this.props.env.account!}:table/${this.props.namePrefix}-laboratory-user-table`],
           actions: ['dynamodb:PutItem'],
           effect: Effect.ALLOW,

cce43d2a34b84a878cc41dfb8b37f9af09bebb92
Zig Tan <zig.tan@twobulls.com>
2024-04-24 16:52:06 +0800

    feat(EG-313): update BE src/main.ts to use shared-lib utils/configuration.ts loadConfiguration()

--- packages/back-end/src/main.ts
+++ packages/back-end/src/main.ts
@@ -1,54 +1,48 @@
-import {
-  validateEasyGenomicsAwsRegion,
-  validateEasyGenomicsEnvSettings,
-  validateEasyGenomicsEnvType,
-} from '@easy-genomics/shared-lib/src/app/utils/common';
+import { join } from 'path';
+import { ConfigurationSettings } from '@easy-genomics/shared-lib/src/app/types/configuration';
+import { loadConfigurations } from '@easy-genomics/shared-lib/src/app/utils/configuration';
 import { App } from 'aws-cdk-lib';
-import dotenv from 'dotenv';
 import { BackEndStack } from './infra/stacks/back-end-stack';
-// eslint-disable-next-line import/no-extraneous-dependencies
-dotenv.config({ path: '.env.local', override: true });
 
-// Validate required Easy Genomics environment settings
-if (validateEasyGenomicsEnvSettings(process)) {
-  // AWS HealthOmics is not available in all regions, so only allow Easy Genomics
-  // web application to be deployed to AWS regions that support AWS HealthOmics.
-  if (validateEasyGenomicsAwsRegion(process.env.AWS_REGION!)) {
-    const app = new App();
+const configurations: {[p: string]: ConfigurationSettings}[] = loadConfigurations(join(__dirname, '../../../config/easy-genomics.yaml'));
+if (configurations.length === 0) {
+  throw new Error('Easy Genomics Configuration(s) missing / invalid');
+} else {
+  const app = new App();
 
-    const envType: string = process.env.ENV_TYPE!.trim().toLowerCase();
-    const subDomain: string = process.env.SUB_DOMAIN!.trim().toLowerCase();
-    const domainName: string = process.env.DOMAIN_NAME!.trim().toLowerCase();
-    const applicationUri: string = envType === 'prod' ? `${subDomain}.${domainName}` : `${subDomain}.${envType}.${domainName}`;
-    const hostedZoneId: string = process.env.HOSTED_ZONE_ID!.trim();
-    const hostedZoneName: string = process.env.HOSTED_ZONE_NAME!.trim().toLowerCase();
-    const certificateArn: string = process.env.CERTIFICATE_ARN!.trim();
+  // Iterate through valid configurations to synthesize CloudFormation stacks
+  configurations.map((configuration: {[p: string]: ConfigurationSettings}) => {
+    const envName: string | undefined = Object.keys(configuration).shift();
+    const configSettings: ConfigurationSettings | undefined = Object.values(configuration).shift();
 
-    if (validateEasyGenomicsEnvType(envType)) {
+    if (envName && configSettings) {
+      const awsAccountId: string = configSettings['aws-account-id'].toString();
+      const awsRegion: string = configSettings['aws-region'];
+      const constructNamespace: string = `${envName}-easy-genomics`;
+      const envType: string = configSettings['env-type']; // dev | pre-prod | prod
+      const subDomain: string | undefined = configSettings['sub-domain'];
+      const systemAdminEmail: string | undefined = configSettings['system-admin-email'];
+
       // AWS infrastructure resources can be destroyed only when devEnv is true
       const devEnv: boolean = envType === 'dev';
       const namePrefix: string = envType === 'prod' ? `${envType}` : `${envType}-${subDomain}`;
 
       // Setups Back-End Stack which initiates the nested stacks for Easy Genomics, AWS HealthOmics and NextFlow Tower
-      new BackEndStack(app, `${subDomain}-main-back-end-stack`, {
-        constructNamespace: `${subDomain}-easy-genomics`,
+      new BackEndStack(app, `${envName}-main-back-end-stack`, {
         env: {
-          account: process.env.AWS_ACCOUNT_ID!,
-          region: process.env.AWS_REGION!,
+          account: awsAccountId,
+          region: awsRegion,
         },
-        envType: envType,
+        constructNamespace: constructNamespace,
         devEnv: devEnv,
+        envName: envName,
+        envType: envType,
         lambdaTimeoutInSeconds: 30,
         namePrefix: namePrefix,
-        siteDistribution: {
-          applicationUri: applicationUri,
-          hostedZoneId: hostedZoneId,
-          hostedZoneName: hostedZoneName,
-          certificateArn: certificateArn,
-        },
+        systemAdminEmail: systemAdminEmail,
       });
     }
+  });
 
-    app.synth();
-  }
+  app.synth();
 }

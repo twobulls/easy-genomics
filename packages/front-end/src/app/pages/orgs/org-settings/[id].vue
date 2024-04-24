4866a2c5e7bcdee3319dffeb9f92dc72302560be
Simon <simon.dib@twobulls.com>
2024-04-24 13:56:00 +1000

    fix(EG-372): fix tabs ui styles not being honoured (#47)

--- packages/front-end/src/app/pages/orgs/org-settings/[id].vue
+++ packages/front-end/src/app/pages/orgs/org-settings/[id].vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
   import { Organization } from '@easy-genomics/shared-lib/src/app/types/easy-genomics/organization';
-  import { UTabsStyles } from '~/styles/nuxtui/UTabs';
 
   const { $api } = useNuxtApp();
   const $route = useRoute();
@@ -38,7 +37,30 @@
   </div>
 
   <UTabs
-    :ui="UTabsStyles"
+    :ui="{
+      base: 'focus:outline-none',
+      list: {
+        base: 'border-b-2 rounded-none  mb-4',
+        padding: 'p-0',
+        height: 'h-14',
+        marker: {
+          wrapper: 'duration-200 ease-out focus:outline-none',
+          base: 'absolute bottom-[0px] h-[2px]',
+          background: 'bg-primary',
+          shadow: 'shadow-none',
+        },
+        size: {
+          sm: 'text-lg',
+        },
+        tab: {
+          base: '!text-base w-auto inline-flex font-heading justify-start ui-focus-visible:outline-0 ui-focus-visible:ring-2 ui-focus-visible:ring-primary-500 ui-not-focus-visible:outline-none focus:outline-none disabled:cursor-not-allowed disabled:opacity-75 duration-200 ease-out',
+          active: 'text-primary h-14',
+          inactive: 'text-heading',
+          height: 'h-14',
+          padding: 'p-0',
+        },
+      },
+    }"
     :default-index="0"
     :items="[
       {

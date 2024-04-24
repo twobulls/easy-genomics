3c01ecf5e9af2b686244d76c02d57e693ff12fc7
Shaun Procter <shaun.procter@deptagency.com>
2024-04-24 22:44:18 +0800

    feat(EG-300): add string-utils.ts file for string manipulation functions

---
+++ packages/front-end/src/app/utils/string-utils.ts
@@ -0,0 +1,44 @@
+// Returns a string conforming to the requirements for entities e.g. Organization name
+export function cleanText(input: string, maxLength: number | undefined) {
+  let cleanedText = removeUnsupportedCharacters(input);
+  cleanedText = singleSpace(cleanedText);
+  cleanedText = firstCharAlphanumeric(cleanedText);
+  cleanedText = maxLengthText(cleanedText, maxLength);
+  return cleanedText;
+}
+
+/*
+  Returns a string containing only alphanumeric characters,
+  hyphen, comma, apostrophe, period, underscore, space and parenthesis (-,'._ )
+*/
+export function removeUnsupportedCharacters(input: string) {
+  return input.replace(/[^a-zA-Z0-9-,'._ ()]/g, '');
+}
+
+// Returns a string with only one space between words
+export function singleSpace(input: string) {
+  return input.replace(/\s+/g, ' ');
+}
+
+// Returns a string with no leading special characters
+export function firstCharAlphanumeric(input: string) {
+  const match = input.match(/[a-zA-Z0-9]/);
+  if (match) {
+    const index = match.index;
+    return input.substring(index);
+  }
+  return '';
+}
+
+/*
+  Returns a string containing the first N-characters of the input string,
+  where N is the maximum number of characters allowed.
+  If maxLength is undefined, returns the original string.
+  If the input string is shorter than maxLength, returns the original string.
+*/
+export function maxLengthText(input: string, maxLength: number | undefined) {
+  if (maxLength) {
+    return input.slice(0, maxLength);
+  }
+  return input;
+}

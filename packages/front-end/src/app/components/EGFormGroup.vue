00c0db5e149c91885560b71533eb743d8966dddf
Shaun Procter <shaun.procter@deptagency.com>
2024-04-24 22:46:28 +0800

    feat(EG-300): add create organization page and form components

---
+++ packages/front-end/src/app/components/EGFormGroup.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  defineProps<{
+    label: string;
+    name: string;
+  }>();
+</script>
+
+<template>
+  <UFormGroup :label="label" :name="name" class="[&_label]:font-normal">
+    <slot />
+  </UFormGroup>
+</template>
+
+<style scoped lang="scss"></style>

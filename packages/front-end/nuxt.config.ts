499810666ce1a57c8d2a95c63dcda86ba7f7e027
Zig Tan <zig.tan@twobulls.com>
2024-04-24 17:33:31 +0800

    feat(EG-391): update FE nuxt.config.ts to use shared-lib configuration.ts loadConfigurations()

--- packages/front-end/nuxt.config.ts
+++ packages/front-end/nuxt.config.ts
@@ -2,7 +2,36 @@
 import IconsResolver from 'unplugin-icons/resolver';
 import Icons from 'unplugin-icons/vite';
 import Components from 'unplugin-vue-components/vite';
+import { ConfigurationSettings } from '@easy-genomics/shared-lib/src/app/types/configuration';
+import { loadConfigurations } from '@easy-genomics/shared-lib/src/app/utils/configuration';
+import { join } from 'path';
 
+if (process.argv.length !== 5 || process.argv[3] !== '--stack' || process.argv[4].trim() === '') {
+  throw new Error('NuxtConfig Setup Error: Missing required arguments --stack {env-name}');
+}
+const stackName: string = process.argv[4].trim();
+
+// Load configuration settings for each environment
+const configurations: {[p: string]: ConfigurationSettings}[] = loadConfigurations(join(__dirname, '../../config/easy-genomics.yaml'));
+if (configurations.length === 0) {
+  throw new Error('Easy Genomics Configuration(s) missing / invalid');
+}
+
+const configuration: {[p: string]: ConfigurationSettings} | undefined = configurations
+  .filter((c: {[p: string]: ConfigurationSettings}) => Object.keys(c).shift() === stackName)
+  .shift();
+if (!configuration) {
+  throw new Error(`Easy Genomics Configuration Settings for "${stackName}" stack not found`);
+}
+
+const envName: string | undefined = Object.keys(configuration).shift();
+const configSettings: ConfigurationSettings | undefined = Object.values(configuration).shift();
+
+if (!envName || !configSettings) {
+  throw new Error(`Easy Genomics Configuration Settings for "${stackName}" stack undefined`);
+}
+
+// @ts-ignore
 export default defineNuxtConfig({
   colorMode: {
     preference: 'light',
@@ -27,11 +56,11 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      AWS_REGION: process.env.AWS_REGION,
-      AWS_USER_POOL_ID: process.env.AWS_USER_POOL_ID,
-      AWS_CLIENT_ID: process.env.AWS_CLIENT_ID,
-      BASE_API_URL: process.env.BASE_API_URL,
-      MOCK_ORG_ID: process.env.MOCK_ORG_ID,
+      AWS_REGION: configSettings['aws-region'],
+      AWS_USER_POOL_ID: configSettings['aws-cognito-user-pool-id'],
+      AWS_CLIENT_ID: configSettings['aws-cognito-client-id'],
+      BASE_API_URL: configSettings['base-api-url'],
+      MOCK_ORG_ID: configSettings['mock-org-id'],
     },
   },
 

e02316394262d7edab489775cb916e413da5f4e2
Shaun Procter <shaun.procter@deptagency.com>
2024-05-06 11:55:46 +0800

    feat(EG-49): implement button variants keeping EGButtonV1 for now

---
+++ packages/front-end/src/app/components/EGButtonV1.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+  type ButtonSize = 'sm' | 'md' | 'lg';
+  type ButtonVariant = 'primary' | 'secondary' | 'ghost' | 'destructive';
+
+  withDefaults(
+    defineProps<{
+      label: string;
+      variant?: ButtonVariant;
+      bold?: boolean;
+      size?: ButtonSize;
+    }>(),
+    {
+      variant: 'primary',
+    }
+  );
+</script>
+
+<template>
+  <!-- TODO: variant styles -->
+  <UButton
+    class="EGButton btn text-body h-12 min-w-24 justify-center rounded-lg px-6 py-2 shadow-none"
+    :class="{
+      'bg-primary hover:bg-primary-dark text-white disabled:bg-gray-400 disabled:opacity-100': variant === 'primary',
+      '': variant === 'secondary',
+      'font-semibold': bold,
+    }"
+  >
+    <slot name="leftIcon" />
+    {{ label }}
+    <slot name="rightIcon" />
+  </UButton>
+</template>
+
+<style scoped lang="scss">
+  .EGButton {
+    transition: 0.2s all ease;
+  }
+</style>

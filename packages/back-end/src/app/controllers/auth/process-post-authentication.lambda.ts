873aa00f248188de567080d6674b195f912d0d7d
Zig Tan <zig.tan@twobulls.com>
2024-05-08 15:31:40 +0800

    feat(EG-386): update comments to clarify authentication-log-table TTL setting

--- packages/back-end/src/app/controllers/auth/process-post-authentication.lambda.ts
+++ packages/back-end/src/app/controllers/auth/process-post-authentication.lambda.ts
@@ -8,9 +8,12 @@ const authenticationLogService = new AuthenticationLogService();
  * This auth lambda function is triggered by the Cognito Post-Authentication Event:
  * https://docs.aws.amazon.com/cognito/latest/developerguide/user-pool-lambda-post-authentication.html
  *
- * It records the user authentication event into the DynamoDB table 'Authentication-Log-Table'
+ * It records the user authentication event into the DynamoDB table 'authentication-log-table'
  * for security auditing purposes, and is automatically cleared by the DynamoDB table's
  * Time-To-Live (TTL) setting.
+ *
+ * NOTE: The 'authentication-log-table' DynamoDB table's TTL setting has not been implemented yet and
+ * will likely require some configuration file changes to support this being a configurable setting.
  *
  * @param event
  */

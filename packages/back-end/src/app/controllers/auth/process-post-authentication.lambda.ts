77aabae655b023da32a4e1f2bfbda49a9704526e
Zig Tan <zig.tan@twobulls.com>
2024-05-08 08:04:08 +0800

    feat(EG-386): add process-post-authentication lambda to log authentication event for tracking

---
+++ packages/back-end/src/app/controllers/auth/process-post-authentication.lambda.ts
@@ -0,0 +1,30 @@
+import { AuthenticationLogEvent } from '@easy-genomics/shared-lib/src/app/types/auth/authentication-log-event';
+import { APIGatewayProxyWithCognitoAuthorizerEvent, Handler } from 'aws-lambda';
+import { AuthenticationLogService } from '../../services/auth/authentication-log-service';
+
+const authenticationLogService = new AuthenticationLogService();
+
+/**
+ * This auth lambda function is triggered by the Cognito Post-Authentication Event:
+ * https://docs.aws.amazon.com/cognito/latest/developerguide/user-pool-lambda-post-authentication.html
+ *
+ * It records the user authentication event into the DynamoDB table 'Authentication-Log-Table'
+ * for security auditing purposes, and is automatically cleared by the DynamoDB table's
+ * Time-To-Live (TTL) setting.
+ *
+ * @param event
+ */
+export const handler: Handler = async (
+  event: APIGatewayProxyWithCognitoAuthorizerEvent,
+): Promise<APIGatewayProxyWithCognitoAuthorizerEvent> => {
+  console.log('EVENT: \n' + JSON.stringify(event, null, 2));
+  // @ts-ignore
+  await authenticationLogService.add(<AuthenticationLogEvent>{
+    UserName: event.userName,
+    DateTime: Date.now(), // UNIX timestamp
+    Event: JSON.stringify(event),
+  }).catch(err => {
+    console.error(err);
+  });
+  return event;
+};

b5623dfe2b44be69151b5449c629290bc7e33236
Zig Tan <zig.tan@twobulls.com>
2024-05-13 12:22:21 +0800

    fix(EG-433): fix comment message

--- packages/shared-lib/src/app/types/easy-genomics/user-invite.d.ts
+++ packages/shared-lib/src/app/types/easy-genomics/user-invite.d.ts
@@ -1,7 +1,7 @@
 /**
  * The following defines the DTO for the FE to submit requests to the
- * /easy-genomics/organization/user/create-user-invite API to invite a
- * new or existing user to an organization.
+ * /easy-genomics/user/create-user-invite API to invite a new or existing user
+ * to an organization.
  *
  * {
  *   OrganizationId: <string>,

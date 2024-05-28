abbc526b50e72fce51cf23df4679fa83ff7b8763
Shaun Procter <shaun.procter@deptagency.com>
2024-05-28 09:27:05 +0800

    feat(EG-294): applied first stash

--- packages/shared-lib/src/app/schema/easy-genomics/laboratory-user-details.ts
+++ packages/shared-lib/src/app/schema/easy-genomics/laboratory-user-details.ts
@@ -1,10 +1,13 @@
 import { z } from 'zod';
 
-export const LaboratoryUserDetailsSchema = z.object({
-  LaboratoryId: z.string().uuid(),
-  UserId: z.string().uuid(),
-  LabManager: z.boolean(),
-  LabTechnician: z.boolean(),
-  UserDisplayName: z.string().optional(),
-  UserEmail: z.string().optional(),
-}).strict();
+export const LaboratoryUserDetailsSchema = z
+  .object({
+    LaboratoryId: z.string().uuid(),
+    UserId: z.string().uuid(),
+    LabManager: z.boolean(),
+    LabTechnician: z.boolean(),
+    UserDisplayName: z.string().optional(),
+    UserEmail: z.string().email().optional(),
+  })
+  .strict();
+export type LaboratoryUserDetails = z.infer<typeof LaboratoryUserDetailsSchema>;

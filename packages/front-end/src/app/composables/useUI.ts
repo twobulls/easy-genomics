4e3fb6a292536d432760ec65490f812446bf9c2b
Shaun Procter <shaun.procter@deptagency.com>
2024-06-05 22:23:22 +0800

    refactor(eg-447): removed debug logging from useUI

--- packages/front-end/src/app/composables/useUI.ts
+++ packages/front-end/src/app/composables/useUI.ts
@@ -2,12 +2,7 @@ import { useUiStore } from '~/stores/stores';
 
 export default function useUI() {
   function isUILoading(isMounted: boolean = true): boolean {
-    const mounted = isMounted;
-    const pending = useUiStore().isRequestPending;
-    const loading = !mounted || pending;
-    console.count('useUI; isUILoading');
-    console.table({ mounted, pending, loading });
-    return loading;
+    return !isMounted || useUiStore().isRequestPending;
   }
 
   return { isUILoading };

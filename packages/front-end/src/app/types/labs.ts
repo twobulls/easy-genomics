663a575fa613de0ef2578c9decc3fc5a3ea3015f
Shaun Procter <shaun.procter@deptagency.com>
2024-06-19 14:06:20 +0800

    feat: update lab details schema and type names for clarity

--- packages/front-end/src/app/types/labs.ts
+++ packages/front-end/src/app/types/labs.ts
@@ -2,24 +2,30 @@ import { z } from 'zod';
 
 export const LabNameSchema = z.string().trim().min(1, 'Lab name is required').max(128, 'Lab name must be no more than 128 characters');
 export const LabDescriptionSchema = z.string().trim().max(500, 'Description must be no longer than 500 characters').optional();
-export const LabNextFlowTowerAccessTokenSchema = z.string().trim().max(128, 'Personal access token must be no more than 128 characters').optional();
-export const LabNextFlowTowerWorkspaceIdSchema = z.string().trim().max(128, 'Workspace ID must be no more than 128 characters').optional();
+export const NextFlowTowerAccessTokenInputSchema = z.string().trim().max(128, 'Personal access token must be no more than 128 characters').optional();
+export const NextFlowTowerWorkspaceIdInputSchema = z.string().trim().max(128, 'Workspace ID must be no more than 128 characters').optional();
 
+// Just the fields required for read-only display
 export const LabDetailsSchema = z.object({
   Name: LabNameSchema,
   Description: LabDescriptionSchema,
-  NextFlowTowerAccessToken: LabNextFlowTowerAccessTokenSchema,
-  NextFlowTowerWorkspaceId: LabNextFlowTowerWorkspaceIdSchema,
+  NextFlowTowerWorkspaceId: NextFlowTowerWorkspaceIdInputSchema,
 });
 
-export const LabDetailsFormSchema = LabDetailsSchema.strict();
+// The fields required for creating a lab
+export const LabDetailsInputSchema = z.object({
+  Name: LabNameSchema,
+  Description: LabDescriptionSchema,
+  NextFlowTowerAccessToken: NextFlowTowerAccessTokenInputSchema,
+  NextFlowTowerWorkspaceId: NextFlowTowerWorkspaceIdInputSchema,
+}).strict();
 
-export type LabDetailsForm = z.infer<typeof LabDetailsFormSchema>;
+export type LabDetailsInput = z.infer<typeof LabDetailsInputSchema>;
 
-const FormModes = {
+const LabDetailsFormModes = {
   Create: 'create',
   Edit: 'edit',
   ReadOnly: 'read-only',
 } as const;
-export const FormModeEnum = z.nativeEnum(FormModes)
-export type FormModeEnum = z.infer<typeof FormModeEnum>;
+export const LabDetailsFormModeEnum = z.nativeEnum(LabDetailsFormModes)
+export type LabDetailsFormModeEnum = z.infer<typeof LabDetailsFormModeEnum>;

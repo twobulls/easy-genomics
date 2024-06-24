923ab8eaf8a2ef30aed6bf7ec4fd0be661d5af4e
Zig Tan <zig.tan@twobulls.com>
2024-06-24 21:57:28 +0800

    feat(EG-426): add Cognito process-pre-token-generation event handler

---
+++ packages/back-end/src/app/controllers/auth/process-pre-token-generation.lambda.ts
@@ -0,0 +1,36 @@
+import { User } from '@easy-genomics/shared-lib/src/app/types/easy-genomics/user';
+import { Handler } from 'aws-lambda';
+import { PreTokenGenerationTriggerEvent } from 'aws-lambda/trigger/cognito-user-pool-trigger/pre-token-generation';
+import { UserService } from '../../services/easy-genomics/user-service';
+
+const userService = new UserService();
+
+/**
+ * This auth lambda function is triggered by the Cognito Pre-Token Generation Event:
+ * https://docs.aws.amazon.com/cognito/latest/developerguide/user-pool-lambda-pre-token-generation.html
+ *
+ * @param event
+ */
+export const handler: Handler = async (
+  event: PreTokenGenerationTriggerEvent,
+): Promise<PreTokenGenerationTriggerEvent> => {
+  console.log('EVENT: \n' + JSON.stringify(event, null, 2));
+
+  // System Admin Account only exists within Cognito User Pool
+  if (process.env.SYSTEM_ADMIN_EMAIL === event.request.userAttributes.email) {
+    return event;
+  }
+
+  const user: User | undefined = (await userService.queryByEmail(event.request.userAttributes.email)).shift();
+  if (user) {
+    event.response = {
+      claimsOverrideDetails: {
+        claimsToAddOrOverride: {
+          ['OrganizationAccess']: JSON.stringify(user.OrganizationAccess),
+        },
+      },
+    };
+  }
+
+  return event;
+};

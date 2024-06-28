5df831ccade332010d8396f417d12b2f948ae5ec
Zig Tan <zig.tan@twobulls.com>
2024-06-28 15:25:30 +0800

    feat(EG-477 | EG-479): add NextFlow Tower API integration & workflows API (#151)
    * feat(EG-477): add shared-lib openapi-typescript dev dependency
    
    * feat(EG-477): add generated nextflow-tower-openapi-spec.d.ts type definition from cloud.seqera.io
    
    * feat(EG-477): add nextflow-tower-api.ts type definition for apis used by Easy Genomics
    
    * feat(EG-477): update nf-tower-nested-stack to automatically detect & register lambdas as REST APIs
    
    * feat(EG-477): update BE configuration to set seqera-api-base-url for self-hosted instance
    
    * feat(EG-477): add rest-api-utils to support communication with Seqera Cloud / NextFlow Tower APIs
    
    * feat(EG-479): add POST /nf-tower/request-list-workflows API
    
    * feat(EG-479): update nf-tower-nested-stack to support POST /nf-tower/request-list-workflows API
    
    * feat(EG-479): update rest-api-utils.ts to add validateOrganizationAccess()
    
    * feat(EG-479): refactor POST request-list-workflows API to GET read-workflows/{labId} API

---
+++ packages/back-end/src/app/controllers/nf-tower/read-workflows.lambda.ts
@@ -0,0 +1,60 @@
+import { ListWorkflowsResponse } from '@easy-genomics/shared-lib/src/app/types/nf-tower/nextflow-tower-api';
+import { buildResponse } from '@easy-genomics/shared-lib/src/app/utils/common';
+import {
+  APIGatewayProxyResult,
+  APIGatewayProxyWithCognitoAuthorizerEvent,
+  Handler,
+} from 'aws-lambda';
+import { LaboratoryService } from '../../services/easy-genomics/laboratory-service';
+import { decrypt } from '../../utils/encryption-utils';
+import { getApiParameters, httpGet, validateOrganizationAccess } from '../../utils/rest-api-utils';
+
+const laboratoryService = new LaboratoryService();
+
+/**
+ * This GET /nf-tower/read-workflows/{LaboratoryId} API queries the /workflow API for
+ * a list of NextFlow Tower Workflows, and it expects:
+ *  - Required Path Parameter containing the LaboratoryId to retrieve the WorkspaceId & AccessToken
+ *  - Optional Query Parameters:
+ *    - max: pagination number of results
+ *    - offset: pagination results offset index
+ *    - search: string to search by the Workflows projectName attribute (e.g. nf-core/viralrecon)
+ * @param event
+ */
+export const handler: Handler = async (
+  event: APIGatewayProxyWithCognitoAuthorizerEvent,
+): Promise<APIGatewayProxyResult> => {
+  console.log('EVENT: \n' + JSON.stringify(event, null, 2));
+  try {
+    // Get Path Parameter
+    const id: string = event.pathParameters?.id || '';
+    if (id === '') throw new Error('Required id is missing');
+
+    const laboratory = await laboratoryService.queryByLaboratoryId(id);
+
+    if (!validateOrganizationAccess(event, laboratory.OrganizationId, laboratory.LaboratoryId)) {
+      throw new Error('Unauthorized');
+    }
+
+    if (!laboratory.NextFlowTowerWorkspaceId) {
+      throw new Error('Laboratory Workspace Id unavailable');
+    }
+    if (!laboratory.NextFlowTowerAccessToken) {
+      throw new Error('Laboratory Access Token unavailable');
+    }
+    const accessToken: string | undefined = await decrypt(laboratory.NextFlowTowerAccessToken);
+
+    // Get Query Parameters for Seqera Cloud / NextFlow Tower APIs
+    const apiParameters = getApiParameters(event);
+    apiParameters.set('workspaceId', `${laboratory.NextFlowTowerWorkspaceId}`);
+
+    const response: ListWorkflowsResponse = await httpGet<ListWorkflowsResponse>(
+      `${process.env.SEQERA_API_BASE_URL}/workflow?${apiParameters.toString()}`,
+      { Authorization: `Bearer ${accessToken}` },
+    );
+    return buildResponse(200, JSON.stringify(response), event);
+  } catch (err: any) {
+    console.error(err);
+    return buildResponse(400, JSON.stringify({ Error: err.message }), event);
+  }
+};

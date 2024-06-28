1d5af5c28bf6cdb98ff745d0623c43a6fe56a1ea
Zig Tan <zig.tan@twobulls.com>
2024-06-28 15:13:51 +0800

    feat(EG-479): update rest-api-utils.ts to add validateOrganizationAccess()

--- packages/back-end/src/app/utils/rest-api-utils.ts
+++ packages/back-end/src/app/utils/rest-api-utils.ts
@@ -1,3 +1,8 @@
+import {
+  LaboratoryAccess, LaboratoryAccessDetails,
+  OrganizationAccess,
+  OrganizationAccessDetails,
+} from '@easy-genomics/shared-lib/src/app/types/easy-genomics/user';
 import { APIGatewayProxyEvent } from 'aws-lambda/trigger/api-gateway-proxy';
 
 /**
@@ -49,4 +54,43 @@ export function getApiParameters(event: APIGatewayProxyEvent): URLSearchParams {
   }
 
   return parameters;
+}
+
+/**
+ * Helper function to check if an authenticated User's JWT OrganizationAccess
+ * metadata allows access to the requested Organization / Laboratory.
+ * @param event
+ * @param organizationId
+ * @param laboratoryId
+ */
+export function validateOrganizationAccess(event: APIGatewayProxyEvent, organizationId: string, laboratoryId?: string): Boolean {
+  try {
+    const orgAccessMetadata: string | undefined = event.requestContext.authorizer?.claims.OrganizationAccess;
+    if (!orgAccessMetadata) {
+      return false;
+    }
+
+    const organizationAccess: OrganizationAccess = JSON.parse(orgAccessMetadata);
+    const organizationAccessDetails: OrganizationAccessDetails | undefined = organizationAccess[organizationId];
+    if (!organizationAccessDetails || (organizationAccessDetails && organizationAccessDetails.Status !== 'Active')) {
+      return false;
+    }
+
+    if (laboratoryId) {
+      const laboratoryAccess: LaboratoryAccess | undefined = organizationAccessDetails.LaboratoryAccess;
+      if (!laboratoryAccess) {
+        return false;
+      }
+
+      const laboratoryAccessDetails: LaboratoryAccessDetails | undefined = laboratoryAccess[laboratoryId];
+      if (!laboratoryAccessDetails || (laboratoryAccessDetails && laboratoryAccessDetails.Status !== 'Active')) {
+        return false;
+      }
+    }
+
+    return true;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
 }

2a56e9d6f2be4215cafc7e40a7072fe355796f19
Zig Tan <zig.tan@twobulls.com>
2024-07-02 19:32:39 +0800

    feat(EG-540): add GET '/nf-tower/list-compute-envs?laboratoryId={LabId}' API

---
+++ packages/back-end/src/app/controllers/nf-tower/list-compute-envs.lambda.ts
@@ -0,0 +1,64 @@
+import { ListComputeEnvsResponse } from '@easy-genomics/shared-lib/src/app/types/nf-tower/nextflow-tower-api';
+import { buildResponse } from '@easy-genomics/shared-lib/src/app/utils/common';
+import { APIGatewayProxyResult, APIGatewayProxyWithCognitoAuthorizerEvent, Handler } from 'aws-lambda';
+import { LaboratoryService } from '../../services/easy-genomics/laboratory-service';
+import { SsmService } from '../../services/ssm-service';
+import { getApiParameters, httpGet, validateOrganizationAccess } from '../../utils/rest-api-utils';
+
+const laboratoryService = new LaboratoryService();
+const ssmService = new SsmService();
+
+/**
+ * This GET /nf-tower/list-compute-envs?laboratoryId={LaboratoryId} API queries
+ * the NextFlow Tower /compute-envs?workspaceId={WorkspaceId} API for a list of
+ * available Compute Environments, and it expects:
+ *  - Required Query Parameter:
+ *    - 'laboratoryId': containing the LaboratoryId to retrieve the WorkspaceId & AccessToken
+ *  - Optional Query Parameters:
+ *    - 'max': pagination number of results
+ *    - 'offset': pagination results offset index
+ *    - 'search': string to search by the Compute-Env name attribute (e.g. ABC)
+ * @param event
+ */
+export const handler: Handler = async (
+  event: APIGatewayProxyWithCognitoAuthorizerEvent,
+): Promise<APIGatewayProxyResult> => {
+  console.log('EVENT: \n' + JSON.stringify(event, null, 2));
+  try {
+    // Get required query parameter
+    const laboratoryId: string = event.queryStringParameters?.laboratoryId || '';
+    if (laboratoryId === '') throw new Error('Required laboratoryId is missing');
+
+    const laboratory = await laboratoryService.queryByLaboratoryId(laboratoryId);
+    if (!validateOrganizationAccess(event, laboratory.OrganizationId, laboratory.LaboratoryId)) {
+      throw new Error('Unauthorized');
+    }
+    if (!laboratory.NextFlowTowerWorkspaceId) {
+      throw new Error('Laboratory Workspace Id unavailable');
+    }
+
+    // Retrieve Seqera Cloud / NextFlow Tower AccessToken from SSM
+    const accessToken: string | undefined = (
+      await ssmService.getParameter({
+        Name: `/easy-genomics/organization/${laboratory.OrganizationId}/laboratory/${laboratory.LaboratoryId}/nf-access-token`,
+        WithDecryption: true,
+      })
+    ).Parameter?.Value;
+    if (!accessToken) {
+      throw new Error('Laboratory Access Token unavailable');
+    }
+
+    // Get Query Parameters for Seqera Cloud / NextFlow Tower APIs
+    const apiParameters: URLSearchParams = getApiParameters(event);
+    apiParameters.set('workspaceId', `${laboratory.NextFlowTowerWorkspaceId}`);
+
+    const response: ListComputeEnvsResponse = await httpGet<ListComputeEnvsResponse>(
+      `${process.env.SEQERA_API_BASE_URL}/compute-envs?${apiParameters.toString()}`,
+      { Authorization: `Bearer ${accessToken}` },
+    );
+    return buildResponse(200, JSON.stringify(response), event);
+  } catch (err: any) {
+    console.error(err);
+    return buildResponse(400, JSON.stringify({ Error: err.message }), event);
+  }
+};

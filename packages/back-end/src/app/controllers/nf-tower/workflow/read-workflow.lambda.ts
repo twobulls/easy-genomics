6f4dcfa6e6284cc0410def9a236a0e182515ae65
Zig Tan <zig.tan@twobulls.com>
2024-07-02 20:46:24 +0800

    feat(EG-536): add GET '/nf-tower/workflow/read-workflow/:id?laboratoryId={LabId}' API

---
+++ packages/back-end/src/app/controllers/nf-tower/workflow/read-workflow.lambda.ts
@@ -0,0 +1,67 @@
+import { DescribeWorkflowResponse } from '@easy-genomics/shared-lib/lib/app/types/nf-tower/nextflow-tower-api';
+import { buildResponse } from '@easy-genomics/shared-lib/lib/app/utils/common';
+import { APIGatewayProxyResult, APIGatewayProxyWithCognitoAuthorizerEvent, Handler } from 'aws-lambda';
+import { LaboratoryService } from '../../../services/easy-genomics/laboratory-service';
+import { SsmService } from '../../../services/ssm-service';
+import { httpGet, validateOrganizationAccess } from '../../../utils/rest-api-utils';
+
+const laboratoryService = new LaboratoryService();
+const ssmService = new SsmService();
+
+/**
+ * This GET /nf-tower/workflow/read-workflow/{:id}?laboratoryId={LaboratoryId}
+ * API queries the NextFlow Tower /workflow/{:id}?workspaceId={WorkspaceId} API
+ * for a specific Workflow (aka Pipeline Run), and it expects:
+ *  - Required Path Parameter:
+ *    - 'id': NextFlow Tower Workflow Id
+ *  - Required Query Parameter:
+ *    - 'laboratoryId': containing the LaboratoryId to retrieve the WorkspaceId & AccessToken
+ *
+ * @param event
+ */
+export const handler: Handler = async (
+  event: APIGatewayProxyWithCognitoAuthorizerEvent,
+): Promise<APIGatewayProxyResult> => {
+  console.log('EVENT: \n' + JSON.stringify(event, null, 2));
+  try {
+    // Get Path Parameter
+    const id: string = event.pathParameters?.id || '';
+    if (id === '') throw new Error('Required id is missing');
+
+    // Get required query parameter
+    const laboratoryId: string = event.queryStringParameters?.laboratoryId || '';
+    if (laboratoryId === '') throw new Error('Required laboratoryId is missing');
+
+    const laboratory = await laboratoryService.queryByLaboratoryId(laboratoryId);
+    if (!validateOrganizationAccess(event, laboratory.OrganizationId, laboratory.LaboratoryId)) {
+      throw new Error('Unauthorized');
+    }
+    if (!laboratory.NextFlowTowerWorkspaceId) {
+      throw new Error('Laboratory Workspace Id unavailable');
+    }
+
+    // Retrieve Seqera Cloud / NextFlow Tower AccessToken from SSM
+    const accessToken: string | undefined = (
+      await ssmService.getParameter({
+        Name: `/easy-genomics/organization/${laboratory.OrganizationId}/laboratory/${laboratory.LaboratoryId}/nf-access-token`,
+        WithDecryption: true,
+      })
+    ).Parameter?.Value;
+    if (!accessToken) {
+      throw new Error('Laboratory Access Token unavailable');
+    }
+
+    // Get Query Parameters for Seqera Cloud / NextFlow Tower APIs
+    const apiParameters: URLSearchParams = new URLSearchParams();
+    apiParameters.set('workspaceId', `${laboratory.NextFlowTowerWorkspaceId}`);
+
+    const response: DescribeWorkflowResponse = await httpGet<DescribeWorkflowResponse>(
+      `${process.env.SEQERA_API_BASE_URL}/workflow/${id}?${apiParameters.toString()}`,
+      { Authorization: `Bearer ${accessToken}` },
+    );
+    return buildResponse(200, JSON.stringify(response), event);
+  } catch (err: any) {
+    console.error(err);
+    return buildResponse(400, JSON.stringify({ Error: err.message }), event);
+  }
+};

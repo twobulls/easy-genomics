0f496b53d46b631c8162ee4ff34e26787220d7a6
Simon Dib <simon.dib@twobulls.com>
2024-07-22 13:24:42 +1000

    refactor: stripNullProperties

--- packages/front-end/src/app/utils/api-utils.ts
+++ packages/front-end/src/app/utils/api-utils.ts
@@ -11,10 +11,10 @@ export function validateApiResponse<T>(schema: ZodSchema<T>, data: any) {
 /**
  * Strip null properties from an array of objects - example usage might be to remove null properties from
  * a Nextflow Tower API response to allow the Zod schema to validate the response
- * @param val
+ * @param items
  */
-export function stripNullProperties(val: Array<any>): Array<any> {
-  return val.map((val) => {
-    return Object.fromEntries(Object.entries(val).filter(([, value]) => value !== null));
+export function stripNullProperties(items: Array<any>): Array<any> {
+  return items.map((item) => {
+    return Object.fromEntries(Object.entries(item).filter(([, value]) => value !== null));
   });
 }

05f92c99cda1acfce1b7737d7a4f2810067c195f
Simon Dib <simon.dib@twobulls.com>
2024-07-22 14:15:26 +1000

    fix: error message

--- packages/front-end/src/app/repository/modules/pipelines.ts
+++ packages/front-end/src/app/repository/modules/pipelines.ts
@@ -17,7 +17,7 @@ class PipelinesModule extends HttpFactory {
     );
 
     if (!res) {
-      throw new Error('Failed to retrieve launch details');
+      throw new Error('Failed to retrieve pipeline launch details');
     }
 
     const cleanedPipelines = stripNullProperties(res?.pipelines || []);

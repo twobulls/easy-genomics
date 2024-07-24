1e172f65eff5ce9b5cf52e22192324869f0fb489
Zig Tan <zig.tan@twobulls.com>
2024-07-24 11:36:56 +0800

    feat(EG-558): add shared-lib s3-file-upload-manifest.ts Zod schema for validation

---
+++ packages/shared-lib/src/app/schema/easy-genomics/upload/s3-file-upload-manifest.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod';
+
+export const UploadFileRequestSchema = z
+  .object({
+    Name: z.string(),
+    Size: z.number(),
+  });
+
+export const RequestFileUploadManifestSchema = z
+  .object({
+    LaboratoryId: z.string().uuid(),
+    Files: z.array(UploadFileRequestSchema),
+  })
+  .strict();

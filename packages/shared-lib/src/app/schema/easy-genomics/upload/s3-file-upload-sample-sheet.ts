1298cc1207c9f8c230d5c354a5fbe5133503e0ab
Zig Tan <zig.tan@twobulls.com>
2024-07-30 13:04:54 +0800

    feat(EG-578): add shared-lib s3-file-upload-sample-sheet.ts zod validation schema

---
+++ packages/shared-lib/src/app/schema/easy-genomics/upload/s3-file-upload-sample-sheet.ts
@@ -0,0 +1,31 @@
+import { z } from 'zod';
+
+// create-file-upload-sample-sheet API request validation schemas
+
+export const UploadedFileInfoSchema = z
+  .object({
+    Name: z.string(),
+    Size: z.number(),
+    Bucket: z.string(),
+    Key: z.string(),
+    Region: z.string(),
+    S3Url: z.string(),
+    S3UrlChecksum: z.string(),
+  })
+  .strict();
+
+// DNA Sequenced R1 & R2 file pair of UploadedFileInfoSchema type
+export const UploadedFilePairInfoSchema = z
+  .object({
+    R1: UploadedFileInfoSchema,
+    R2: UploadedFileInfoSchema,
+  })
+  .strict();
+
+export const SampleSheetRequestSchema = z
+  .object({
+    LaboratoryId: z.string().uuid(),
+    TransactionId: z.string().uuid(),
+    UploadedFilePairs: z.array(UploadedFilePairInfoSchema),
+  })
+  .strict();

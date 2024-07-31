e99dd855de1e9187d4a6d70781bd77f02d398a99
Simon <simon.dib@twobulls.com>
2024-07-31 16:22:10 +1000

    feat(EG-559): parameters step (pipeline run step 3) (#214)
    * feat: step3 workflow complete
    
    * feat: reset pipeline cleanly after success
    
    * style: build info overlay opacity
    
    was obscuring UI
    
    * refactor: cleanup
    
    * chore: comment

---
+++ packages/front-end/src/app/components/EGRunPipelineFormEditParameters.vue
@@ -0,0 +1,136 @@
+<script setup lang="ts">
+  import { ButtonSizeEnum } from '~/types/buttons';
+
+  const props = defineProps<{
+    schema: object;
+    params: object;
+  }>();
+
+  const { $api } = useNuxtApp();
+  const emit = defineEmits(['previous-step', 'step-validated']);
+
+  const activeSection = ref<string | null>(null);
+
+  const localProps = reactive({
+    schema: props.schema,
+    params: props.params,
+  });
+
+  onMounted(() => {
+    // set first section in side panel of UI to active
+    const definitions = props.schema.definitions;
+    const firstKey = Object.keys(definitions)[0];
+    activeSection.value = definitions[firstKey].title;
+  });
+
+  function nextSection() {
+    const currentIndex = Object.values(props.schema.definitions).findIndex(
+      (section) => section.title == activeSection.value,
+    );
+    if (currentIndex < Object.values(props.schema.definitions).length - 1) {
+      activeSection.value = Object.values(props.schema.definitions)[currentIndex + 1].title;
+    }
+  }
+
+  function prevSection() {
+    if (!props.schema || !props.schema.definitions) {
+      return; // Exit if schema or definitions is not available
+    }
+    const definitionsArray = Object.values(props.schema.definitions);
+    const currentIndex = definitionsArray.findIndex((section) => section.title === activeSection.value);
+
+    if (currentIndex > 0) {
+      activeSection.value = definitionsArray[currentIndex - 1].title;
+    }
+  }
+
+  function toggleSection(sectionTitle: string) {
+    if (activeSection.value !== sectionTitle) {
+      activeSection.value = sectionTitle;
+    }
+  }
+
+  watch(
+    // watches for input changes in the local params object and updates the store with the new value
+    () => localProps.params,
+    (val) => {
+      if (val) {
+        usePipelineRunStore().setParams(val);
+      }
+    },
+    { deep: true },
+  );
+</script>
+
+<template>
+  <div class="flex">
+    <div class="mr-4 w-1/4">
+      <EGCard>
+        <EGText tag="small" class="mb-4">Step 03</EGText>
+        <EGText tag="h4" class="mb-0">Edit Parameters</EGText>
+        <UDivider class="py-4" />
+        <div v-for="(section, sectionIndex) in schema.definitions" :key="`section-${sectionIndex}`">
+          <div
+            class="mb-4 cursor-pointer text-sm"
+            :class="{
+              'text-primary bg-primary-muted rounded-xl px-4 py-2 font-semibold': activeSection === section.title,
+            }"
+            @click="toggleSection(section.title)"
+          >
+            {{ section.title }}
+          </div>
+        </div>
+      </EGCard>
+    </div>
+    <div class="w-3/4">
+      <EGCard
+        v-for="(section, sectionIndex) in Object.values(schema.definitions)"
+        :key="`section-${sectionIndex}`"
+        v-show="activeSection === section.title"
+      >
+        <EGText tag="h4" class="mb-4">{{ section.title }}</EGText>
+
+        <EGRunPipelineParameterInputs
+          v-for="(section, sectionIndex) in Object.values(schema.definitions)"
+          :key="`section-${sectionIndex}`"
+          v-show="activeSection === section.title"
+          :section="<Object>section"
+          :params="localProps.params"
+          @update:params="
+            (val) => {
+              localProps.params = val;
+            }
+          "
+        />
+
+        <div class="mt-12 flex justify-end">
+          <EGButton
+            v-if="sectionIndex > 0"
+            :size="ButtonSizeEnum.enum.sm"
+            variant="secondary"
+            label="Previous"
+            @click="prevSection"
+          />
+          <EGButton
+            class="ml-4"
+            v-if="sectionIndex < Object.keys(schema.definitions).length - 1"
+            :size="ButtonSizeEnum.enum.sm"
+            variant="secondary"
+            label="Next"
+            @click="nextSection"
+          />
+        </div>
+      </EGCard>
+
+      <div class="mt-6 flex justify-between">
+        <EGButton
+          :size="ButtonSizeEnum.enum.sm"
+          variant="secondary"
+          label="Previous step"
+          @click="emit('previous-step')"
+        />
+        <EGButton :size="ButtonSizeEnum.enum.sm" type="submit" label="Save & Continue" @click="emit('next-step')" />
+      </div>
+    </div>
+  </div>
+</template>
---
+++ packages/front-end/src/app/pages/labs/[labId]/[pipelineId]/run-pipeline.vue
@@ -0,0 +1,106 @@
+<script setup lang="ts">
+  import { usePipelineRunStore } from '~/stores';
+  import { ButtonVariantEnum } from '~/types/buttons';
+  import { initialize } from 'esbuild';
+
+  const { $api } = useNuxtApp();
+  const $router = useRouter();
+  const $route = useRoute();
+  const labName = usePipelineRunStore().labName;
+
+  const isDialogOpen = ref(false);
+  const hasLaunched = ref(false);
+  const exitConfirmed = ref(false);
+  const backNavigationInProgress = ref(false);
+  const nextRoute = ref(null);
+  const schema = ref('');
+  const resetStepperKey = ref(0);
+
+  onBeforeMount(async () => {
+    await initializePipelineData();
+  });
+
+  /**
+   * Intercept any navigation away from the page (including the browser back button) and present the modal
+   */
+  onBeforeRouteLeave((to, from, next) => {
+    if (hasLaunched.value) next(true);
+    else if (!exitConfirmed.value) {
+      handleExitRun();
+      nextRoute.value = to.path;
+      next(false);
+    } else {
+      next(true);
+    }
+  });
+
+  /**
+   * Reads the pipeline schema and parameters from the API and initializes the pipeline run store
+   */
+  async function initializePipelineData() {
+    const res = await $api.pipelines.readPipelineSchema($route.params.pipelineId, $route.params.labId);
+    schema.value = JSON.parse(res.schema);
+    usePipelineRunStore().setParams(JSON.parse(<string>res.params));
+  }
+
+  function handleDialogAction() {
+    exitConfirmed.value = true;
+    isDialogOpen.value = false;
+    backNavigationInProgress.value = true;
+    $router.go(-1);
+    backNavigationInProgress.value = false;
+  }
+
+  function handleExitRun() {
+    isDialogOpen.value = true;
+  }
+
+  /**
+   * Resets the pipeline run workflow:
+   * - clears some store values
+   * - re-initializes the schema + prefills params
+   * - re-mounts the stepper to reset it to initial state
+   */
+  function resetRunPipeline() {
+    usePipelineRunStore().setUserPipelineRunName('');
+    usePipelineRunStore().setParams({});
+    initializePipelineData();
+    resetStepperKey.value++;
+  }
+
+  watch([isDialogOpen, backNavigationInProgress], ([dialogOpen, navigatingBack]) => {
+    if (dialogOpen) {
+      nextRoute.value = null;
+      return; // If the dialog is still open, return and don't execute the routing logic
+    }
+    if (!navigatingBack && nextRoute.value && isDialogOpen.value) {
+      $router.push(nextRoute.value);
+      nextRoute.value = null;
+    }
+  });
+</script>
+
+<template>
+  <EGPageHeader
+    title="Run Pipeline"
+    :description="labName"
+    :show-back-button="!hasLaunched"
+    :back-button-action="handleExitRun"
+    back-button-label="Exit Run"
+  />
+  <EGRunPipelineStepper
+    @has-launched="hasLaunched = true"
+    :schema="schema"
+    :params="usePipelineRunStore().params"
+    @reset-run-pipeline="resetRunPipeline()"
+    :key="resetStepperKey"
+  />
+  <EGDialog
+    action-label="Cancel Pipeline Run"
+    :action-variant="ButtonVariantEnum.enum.destructive"
+    @action-triggered="handleDialogAction"
+    primary-message="Are you sure you would like to cancel?"
+    secondary-message="Any changes made or files uploaded will not be saved."
+    v-model="isDialogOpen"
+  />
+</template>

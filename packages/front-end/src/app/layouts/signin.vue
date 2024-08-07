3d8351a39821f92c7e4a32e3c34b2953be52670e
Shaun Procter <shaun.procter@deptagency.com>
2024-08-07 10:45:12 +0800

    chore(EG-442): remove phone number from sign-in page

--- packages/front-end/src/app/layouts/signin.vue
+++ packages/front-end/src/app/layouts/signin.vue
@@ -16,9 +16,8 @@
     <div class="flex min-h-screen flex-col">
       <div class="flex flex-grow flex-col items-center justify-center"><slot /></div>
       <footer
-        class="border-stroke-light flex w-full justify-between gap-5 border-t border-solid px-12 py-6 text-center text-sm leading-5"
+        class="border-stroke-light flex w-full justify-end gap-5 border-t border-solid px-12 py-6 text-center text-sm leading-5"
       >
-        <EGText tag="a" color-class="text-muted" href="tel:+15551234567">Call +1 (555) 123-4567</EGText>
         <EGText tag="a" color-class="text-muted" href="mailto:contact@easygenomics.com">Contact Easy Genomics</EGText>
       </footer>
     </div>

b2eab079f2d13822dda125a062a18d24ab9269f7
Zig Tan <zig.tan@twobulls.com>
2024-08-08 13:08:37 +0800

    feat(EG-606): add validation checks for FE Nuxt Settings

--- packages/front-end/src/main.ts
+++ packages/front-end/src/main.ts
@@ -17,6 +17,9 @@ if (process.env.CI_CD === 'true') {
   const appDomainName: string | undefined = process.env.APP_DOMAIN_NAME;
   const awsHostedZoneId: string | undefined = process.env.AWS_HOSTED_ZONE_ID;
 
+  const awsApiGatewayUrl: string | undefined = process.env.AWS_API_GATEWAY_URL;
+  const awsCognitoUserPoolId: string | undefined = process.env.AWS_COGNITO_USER_POOL_ID;
+  const awsCognitoUserPoolClientId: string | undefined = process.env.AWS_COGNITO_USER_POOL_CLIENT_ID;
   const awsCertificateArn: string | undefined = process.env.AWS_CERTIFICATE_ARN;
 
   // AWS infrastructure resources can be destroyed only when devEnv is true
@@ -29,6 +32,16 @@ if (process.env.CI_CD === 'true') {
   if (!appDomainName) throw new Error('APP_DOMAIN_NAME undefined, please check the CI/CD environment configuration');
   if (!devEnv && !awsHostedZoneId) {
     throw new Error('AWS_HOSTED_ZONE_ID undefined, please check the CI/CD environment configuration');
+  }
+
+  if (!awsApiGatewayUrl) {
+    throw new Error('AWS_API_GATEWAY_URL undefined, please check the CI/CD environment configuration');
+  }
+  if (!awsCognitoUserPoolId) {
+    throw new Error('AWS_COGNITO_USER_POOL_ID undefined, please check the CI/CD environment configuration');
+  }
+  if (!awsCognitoUserPoolClientId) {
+    throw new Error('AWS_COGNITO_USER_POOL_CLIENT_ID undefined, please check the CI/CD environment configuration');
   }
   if (!awsCertificateArn) {
     throw new Error('AWS_CERTIFICATE_ARN undefined, please check the CI/CD environment configuration');
@@ -74,12 +87,28 @@ if (process.env.CI_CD === 'true') {
       const awsHostedZoneId: string | undefined = configSettings['aws-hosted-zone-id'];
 
       // Front-End configuration settings
+      const awsApiGatewayUrl: string | undefined = configSettings['front-end']['aws-api-gateway-url'];
+      const awsCognitoUserPoolId: string | undefined = configSettings['front-end']['aws-cognito-user-pool-id'];
+      const awsCognitoUserPoolClientId: string | undefined =
+        configSettings['front-end']['aws-cognito-user-pool-client-id'];
+
       const awsCertificateArn: string = configSettings['front-end']['aws-certificate-arn'];
 
       // AWS infrastructure resources can be destroyed only when devEnv is true
       const devEnv: boolean = envType === 'dev';
       const constructNamespace: string = `${envName}-easy-genomics`;
 
+      if (!awsApiGatewayUrl) {
+        throw new Error('"aws-api-gateway-url" undefined, please check the easy-genomics.yaml configuration');
+      }
+      if (!awsCognitoUserPoolId) {
+        throw new Error('"aws-cognito-user-pool-id" undefined, please check the easy-genomics.yaml configuration');
+      }
+      if (!awsCognitoUserPoolClientId) {
+        throw new Error(
+          '"aws-cognito-user-pool-client-id" undefined, please check the easy-genomics.yaml configuration',
+        );
+      }
       if (!devEnv && !awsHostedZoneId) {
         throw new Error('"aws-hosted-zone-id" undefined, please check the easy-genomics.yaml configuration');
       }

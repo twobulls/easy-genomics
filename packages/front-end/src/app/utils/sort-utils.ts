13f9a7c8013933bd70e9795df6fe7e52db76f200
Andrew Purcell <andrew.purcell@deptagency.com>
2024-08-23 04:32:03 +1000

    fix: fall back on case sensitive search if insensitive strings match

--- packages/front-end/src/app/utils/sort-utils.ts
+++ packages/front-end/src/app/utils/sort-utils.ts
@@ -1,14 +1,14 @@
 /**
- * Validates a single field value against a given schema and adds any resulting validation errors to the provided errors array.
- * This function is used to validate individual fields in the form, allowing for custom validation logic per field.
- *
- * @param errors - The array to which any discovered validation errors will be added.
- * @param schema - The Zod schema against which the field value will be validated.
- * @param fieldName - The name of the field being validated. Used to associate errors with specific form fields.
- * @param fieldValue - The value of the field being validated. Can be undefined, in which case the schema determines validity.
+ * An insensitive case sort function for use by Nuxt UI tables
  */
 export function caseInsensitiveSortFn(a: any, b: any, direction: 'asc' | 'desc') {
   if (a.toLowerCase() === b.toLowerCase()) {
+    // If idenical ignoring case, sort by case for consistency of results
+    if (direction === 'asc') {
+      return a < b ? -1 : 1;
+    } else {
+      return a > b ? -1 : 1;
+    }
     return 0;
   }
   if (direction === 'asc') {

7e4a7d6be85468dc850fe9697599cd55ce3e2a6e
Zig Tan <zig.tan@twobulls.com>
2024-08-28 12:12:19 +0800

    feat(EG-666): simplify FE nuxt.config.ts to read settings from environment variables

--- packages/front-end/nuxt.config.ts
+++ packages/front-end/nuxt.config.ts
@@ -1,76 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import path from 'path';
-import { join } from 'path';
-import { ConfigurationSettings } from '@easy-genomics/shared-lib/src/app/types/configuration';
-import {
-  findConfiguration,
-  getStackEnvName,
-  loadConfigurations,
-} from '@easy-genomics/shared-lib/src/app/utils/configuration';
 import IconsResolver from 'unplugin-icons/resolver';
 import Icons from 'unplugin-icons/vite';
 import Components from 'unplugin-vue-components/vite';
 
-let awsRegion: string | undefined;
-let awsApiGatewayUrl: string | undefined;
-let awsCognitoUserPoolId: string | undefined;
-let awsCognitoUserPoolClientId: string | undefined;
+/**
+ * Read the following environmental variables setup either by:
+ *  - CI/CD release environment, or
+ *  - front-end/src/main.ts logic using the easy-genomics.yaml configuration file
+ */
+const awsRegion: string | undefined = process.env.AWS_REGION;
+const envType: string | undefined = process.env.ENV_TYPE;
+const awsApiGatewayUrl: string | undefined = process.env.AWS_API_GATEWAY_URL;
+const awsCognitoUserPoolId: string | undefined = process.env.AWS_COGNITO_USER_POOL_ID;
+const awsCognitoUserPoolClientId: string | undefined = process.env.AWS_COGNITO_USER_POOL_CLIENT_ID;
 
-if (process.env.CI_CD === 'true') {
-  console.log('Loading Front-End Nuxt environment settings for CI/CD Pipeline...');
-
-  // CI/CD Pipeline uses ENV parameters
-  awsRegion = process.env.AWS_REGION;
-  awsApiGatewayUrl = process.env.AWS_API_GATEWAY_URL;
-  awsCognitoUserPoolId = process.env.AWS_COGNITO_USER_POOL_ID;
-  awsCognitoUserPoolClientId = process.env.AWS_COGNITO_USER_POOL_CLIENT_ID;
-
+if (
+  process.env.npm_lifecycle_script === 'nuxt dev' ||
+  process.env.npm_lifecycle_script === 'nuxt generate' ||
+  process.env.npm_lifecycle_script === 'nuxt build'
+) {
+  if (!awsRegion) {
+    throw new Error('AWS_REGION undefined, please check the environment configuration');
+  }
+  if (!envType) {
+    throw new Error('ENV_TYPE undefined, please check the environment configuration');
+  }
   if (!awsApiGatewayUrl) {
-    throw new Error('AWS_API_GATEWAY_URL undefined, please check the CI/CD environment configuration');
+    throw new Error('AWS_API_GATEWAY_URL undefined, please check the environment configuration');
   }
   if (!awsCognitoUserPoolId) {
-    throw new Error('AWS_COGNITO_USER_POOL_ID undefined, please check the CI/CD environment configuration');
+    throw new Error('AWS_COGNITO_USER_POOL_ID undefined, please check the environment configuration');
   }
   if (!awsCognitoUserPoolClientId) {
-    throw new Error('AWS_COGNITO_USER_POOL_CLIENT_ID undefined, please check the CI/CD environment configuration');
-  }
-} else {
-  console.log('Loading Front-End Nuxt easy-genomics.yaml settings...');
-
-  // Load configuration settings for each environment
-  const configurations: { [p: string]: ConfigurationSettings }[] = loadConfigurations(
-    join(__dirname, '../../config/easy-genomics.yaml'),
-  );
-  if (configurations.length === 0) {
-    throw new Error('Easy Genomics Configuration(s) missing / invalid, please update: easy-genomics.yaml');
-  }
-
-  // Try to retrieve optional command argument: --stack {env-name}
-  const STACK_ENV_NAME: string | undefined = getStackEnvName();
-  if (configurations.length > 1 && !STACK_ENV_NAME) {
-    throw new Error('Multiple configurations found in easy-genomics.yaml, please specify argument: --stack {env-name}');
-  }
-
-  const configuration: { [p: string]: ConfigurationSettings } =
-    configurations.length > 1 ? findConfiguration(STACK_ENV_NAME!, configurations) : configurations.shift()!;
-  const configSettings = Object.values(configuration).shift();
-  if (!configSettings) {
-    throw new Error('Easy Genomics Configuration(s) missing / invalid, please update: easy-genomics.yaml');
-  }
-
-  awsRegion = configSettings['aws-region'];
-  awsApiGatewayUrl = configSettings['front-end']['aws-api-gateway-url'];
-  awsCognitoUserPoolId = configSettings['front-end']['aws-cognito-user-pool-id'];
-  awsCognitoUserPoolClientId = configSettings['front-end']['aws-cognito-user-pool-client-id'];
-
-  if (!awsApiGatewayUrl) {
-    throw new Error('"aws-api-gateway-url" undefined, please check the easy-genomics.yaml configuration');
-  }
-  if (!awsCognitoUserPoolId) {
-    throw new Error('"aws-cognito-user-pool-id" undefined, please check the easy-genomics.yaml configuration');
-  }
-  if (!awsCognitoUserPoolClientId) {
-    throw new Error('"aws-cognito-user-pool-client-id" undefined, please check the easy-genomics.yaml configuration');
+    throw new Error('AWS_COGNITO_USER_POOL_CLIENT_ID undefined, please check the environment configuration');
   }
 }
 
@@ -99,8 +62,8 @@ export default defineNuxtConfig({
       AWS_REGION: awsRegion,
       AWS_USER_POOL_ID: awsCognitoUserPoolId,
       AWS_CLIENT_ID: awsCognitoUserPoolClientId,
-      BASE_API_URL: awsApiGatewayUrl.replace(/\/+$/, ''), // Remove trailing slashes
-      ENV_TYPE: process.env.envType || 'dev',
+      BASE_API_URL: awsApiGatewayUrl?.replace(/\/+$/, ''), // Remove trailing slashes
+      ENV_TYPE: envType || 'dev',
       GITHUB_RUN_NUMBER: process.env.GITHUB_RUN_NUMBER || 'Unknown',
     },
   },

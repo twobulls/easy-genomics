d532d024498456218830be1bce4014104fc3c2d8
Zig Tan <zig.tan@twobulls.com>
2024-08-28 09:50:26 +0800

    feat(EG-666): update FE main.ts to extend config setting validations

--- packages/front-end/src/main.ts
+++ packages/front-end/src/main.ts
@@ -16,32 +16,28 @@ if (process.env.CI_CD === 'true') {
   const envType: string | undefined = process.env.ENV_TYPE;
   const appDomainName: string | undefined = process.env.APP_DOMAIN_NAME;
   const awsHostedZoneId: string | undefined = process.env.AWS_HOSTED_ZONE_ID;
-
-  const awsApiGatewayUrl: string | undefined = process.env.AWS_API_GATEWAY_URL;
-  const awsCognitoUserPoolId: string | undefined = process.env.AWS_COGNITO_USER_POOL_ID;
-  const awsCognitoUserPoolClientId: string | undefined = process.env.AWS_COGNITO_USER_POOL_CLIENT_ID;
   const awsCertificateArn: string | undefined = process.env.AWS_CERTIFICATE_ARN;
 
   // AWS infrastructure resources can be destroyed only when devEnv is true
   const devEnv: boolean = envType === 'dev';
 
-  if (!envName) throw new Error('ENV_NAME undefined, please check the CI/CD environment configuration');
-  if (!awsAccountId) throw new Error('AWS_ACCOUNT_ID undefined, please check the CI/CD environment configuration');
-  if (!awsRegion) throw new Error('AWS_REGION undefined, please check the CI/CD environment configuration');
-  if (!envType) throw new Error('ENV_TYPE undefined, please check the CI/CD environment configuration');
-  if (!appDomainName) throw new Error('APP_DOMAIN_NAME undefined, please check the CI/CD environment configuration');
+  if (!envName) {
+    throw new Error('ENV_NAME undefined, please check the CI/CD environment configuration');
+  }
+  if (!awsAccountId) {
+    throw new Error('AWS_ACCOUNT_ID undefined, please check the CI/CD environment configuration');
+  }
+  if (!awsRegion) {
+    throw new Error('AWS_REGION undefined, please check the CI/CD environment configuration');
+  }
+  if (!envType) {
+    throw new Error('ENV_TYPE undefined, please check the CI/CD environment configuration');
+  }
+  if (!appDomainName) {
+    throw new Error('APP_DOMAIN_NAME undefined, please check the CI/CD environment configuration');
+  }
   if (!devEnv && !awsHostedZoneId) {
     throw new Error('AWS_HOSTED_ZONE_ID undefined, please check the CI/CD environment configuration');
-  }
-
-  if (!awsApiGatewayUrl) {
-    throw new Error('AWS_API_GATEWAY_URL undefined, please check the CI/CD environment configuration');
-  }
-  if (!awsCognitoUserPoolId) {
-    throw new Error('AWS_COGNITO_USER_POOL_ID undefined, please check the CI/CD environment configuration');
-  }
-  if (!awsCognitoUserPoolClientId) {
-    throw new Error('AWS_COGNITO_USER_POOL_CLIENT_ID undefined, please check the CI/CD environment configuration');
   }
   if (!devEnv && !awsCertificateArn) {
     throw new Error('AWS_CERTIFICATE_ARN undefined, please check the CI/CD environment configuration');
@@ -50,7 +46,6 @@ if (process.env.CI_CD === 'true') {
   const constructNamespace: string = `${envName}-easy-genomics`;
 
   // Setups Front-End Stack to support static web hosting for the UI
-
   new FrontEndStack(app, `${envName}-main-front-end-stack`, {
     env: {
       account: awsAccountId,
@@ -86,29 +81,23 @@ if (process.env.CI_CD === 'true') {
       const envType: string = configSettings['env-type']; // dev | pre-prod | prod
       const appDomainName: string = configSettings['app-domain-name'];
       const awsHostedZoneId: string | undefined = configSettings['aws-hosted-zone-id'];
-
-      // Front-End configuration settings
-      const awsApiGatewayUrl: string | undefined = configSettings['front-end']['aws-api-gateway-url'];
-      const awsCognitoUserPoolId: string | undefined = configSettings['front-end']['aws-cognito-user-pool-id'];
-      const awsCognitoUserPoolClientId: string | undefined =
-        configSettings['front-end']['aws-cognito-user-pool-client-id'];
-
-      const awsCertificateArn: string | undefined = configSettings['front-end']['aws-certificate-arn'];
+      const awsCertificateArn: string | undefined = configSettings['aws-certificate-arn'];
 
       // AWS infrastructure resources can be destroyed only when devEnv is true
       const devEnv: boolean = envType === 'dev';
       const constructNamespace: string = `${envName}-easy-genomics`;
 
-      if (!awsApiGatewayUrl) {
-        throw new Error('"aws-api-gateway-url" undefined, please check the easy-genomics.yaml configuration');
+      if (!awsAccountId) {
+        throw new Error('"aws-account-id" undefined, please check the easy-genomics.yaml configuration');
       }
-      if (!awsCognitoUserPoolId) {
-        throw new Error('"aws-cognito-user-pool-id" undefined, please check the easy-genomics.yaml configuration');
+      if (!awsRegion) {
+        throw new Error('"aws-region" undefined, please check the easy-genomics.yaml configuration');
       }
-      if (!awsCognitoUserPoolClientId) {
-        throw new Error(
-          '"aws-cognito-user-pool-client-id" undefined, please check the easy-genomics.yaml configuration',
-        );
+      if (!envType) {
+        throw new Error('"env-type" undefined, please check the easy-genomics.yaml configuration');
+      }
+      if (!appDomainName) {
+        throw new Error('"app-domain-name" undefined, please check the easy-genomics.yaml configuration');
       }
       if (!devEnv && !awsHostedZoneId) {
         throw new Error('"aws-hosted-zone-id" undefined, please check the easy-genomics.yaml configuration');
@@ -118,7 +107,6 @@ if (process.env.CI_CD === 'true') {
       }
 
       // Setups Front-End Stack to support static web hosting for the UI
-
       new FrontEndStack(app, `${envName}-main-front-end-stack`, {
         env: {
           account: awsAccountId,

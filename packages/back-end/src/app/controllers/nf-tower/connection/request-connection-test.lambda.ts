c86a1486e04211b8c92617dbbbba7bd1e9827932
Andrew Purcell <andrew.purcell@deptagency.com>
2024-09-02 10:05:18 +1000

    feat: changed error response

--- packages/back-end/src/app/controllers/nf-tower/connection/request-connection-test.lambda.ts
+++ packages/back-end/src/app/controllers/nf-tower/connection/request-connection-test.lambda.ts
@@ -1,6 +1,6 @@
 import { RequestNFConnectionTestSchema } from '@easy-genomics/shared-lib/src/app/schema/nf-tower/connection-test';
+import { NFConnectionTest } from '@easy-genomics/shared-lib/src/app/types/nf-tower/connection-test';
 import { ListComputeEnvsResponse } from '@easy-genomics/shared-lib/src/app/types/nf-tower/nextflow-tower-api';
-import { NFConnectionTest } from '@easy-genomics/shared-lib/src/app/types/nf-tower/nextflow-tower-connection-test';
 import { buildResponse } from '@easy-genomics/shared-lib/src/app/utils/common';
 import { APIGatewayProxyResult, APIGatewayProxyWithCognitoAuthorizerEvent, Handler } from 'aws-lambda';
 import { httpRequest, REST_API_METHOD } from '@BE/utils/rest-api-utils';
@@ -33,12 +33,7 @@ export const handler: Handler = async (
     return buildResponse(200, JSON.stringify({ Status: 'Success' }), event);
   } catch (err: any) {
     console.error(err);
-    return {
-      statusCode: 400,
-      body: JSON.stringify({
-        Error: getErrorMessage(err),
-      }),
-    };
+    return buildResponse(400, JSON.stringify({ Error: getErrorMessage(err) }), event);
   }
 };
 

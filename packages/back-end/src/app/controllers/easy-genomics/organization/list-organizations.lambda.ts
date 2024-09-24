97e665ebc93e26c56e466073037b04aa369d091f
Zig Tan <zig.tan@twobulls.com>
2024-09-24 10:37:19 +0700

    feat(EG-704): update list-organization API for SysAdmin/User filtering (#299)
    * feat(EG-704): update auth-utils to add getOrganizationAccessOrganizationIds()
    
    * feat(EG-704): update list-organizations API to support SysAdmin / normal User filtering
    
    * feat(EG-704): remove exception if user's Organizations access list is empty

--- packages/back-end/src/app/controllers/easy-genomics/organization/list-organizations.lambda.ts
+++ packages/back-end/src/app/controllers/easy-genomics/organization/list-organizations.lambda.ts
@@ -2,16 +2,38 @@ import { Organization } from '@easy-genomics/shared-lib/src/app/types/easy-genom
 import { buildResponse } from '@easy-genomics/shared-lib/src/app/utils/common';
 import { APIGatewayProxyResult, APIGatewayProxyWithCognitoAuthorizerEvent, Handler } from 'aws-lambda';
 import { OrganizationService } from '@BE/services/easy-genomics/organization-service';
+import { getOrganizationAccessOrganizationIds, validateSystemAdminAccess } from '@BE/utils/auth-utils';
 
 const organizationService = new OrganizationService();
 
+/**
+ * This API is returns a list of Organizations depending on the User's Cognito
+ * Session.
+ *
+ * If the User is the SystemAdmin, then it will return all Organizations in the
+ * platform. Otherwise if it is a normal User, then it will return only the
+ * Organizations the User has been invited to.
+ *
+ * @param event
+ */
 export const handler: Handler = async (
   event: APIGatewayProxyWithCognitoAuthorizerEvent,
 ): Promise<APIGatewayProxyResult> => {
   console.log('EVENT: \n' + JSON.stringify(event, null, 2));
   try {
-    const response: Organization[] = await organizationService.list();
-    return buildResponse(200, JSON.stringify(response), event);
+    if (validateSystemAdminAccess(event)) {
+      const response: Organization[] = await organizationService.list();
+      return buildResponse(200, JSON.stringify(response), event);
+    } else {
+      const organizationIds = getOrganizationAccessOrganizationIds(event);
+
+      const response: Organization[] = await organizationService.list();
+      const userOrganizations: Organization[] = response.filter((org: Organization) =>
+        organizationIds.includes(org.OrganizationId),
+      );
+
+      return buildResponse(200, JSON.stringify(userOrganizations), event);
+    }
   } catch (err: any) {
     console.error(err);
     return buildResponse(400, JSON.stringify({ Error: getErrorMessage(err) }), event);

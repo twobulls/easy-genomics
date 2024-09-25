9865afd0c87034f5f4efc42726416238ec57fc06
William Holt <william.holt@twobulls.com>
2024-09-26 09:43:38 +1000

    refactor: rework pipeline run navigate-out confirmation logic

--- packages/front-end/src/app/pages/labs/[labId]/[pipelineId]/run-pipeline.vue
+++ packages/front-end/src/app/pages/labs/[labId]/[pipelineId]/run-pipeline.vue
@@ -7,13 +7,10 @@
   const $route = useRoute();
   const labName = usePipelineRunStore().labName;
 
-  const isDialogOpen = ref<boolean>(false);
   const hasLaunched = ref<boolean>(false);
   const exitConfirmed = ref<boolean>(false);
-  const backNavigationInProgress = ref<boolean>(false);
   const nextRoute = ref<string | null>(null);
   const schema = ref({});
-  const resetStepperKey = ref(0);
 
   onBeforeMount(async () => {
     await initializePipelineData();
@@ -23,12 +20,18 @@
    * Intercept any navigation away from the page (including the browser back button) and present the modal
    */
   onBeforeRouteLeave((to, from, next) => {
-    if (hasLaunched.value) next(true);
-    else if (!exitConfirmed.value) {
-      handleExitRun();
+    if (hasLaunched.value) {
+      // if the pipeline has launched no need to confirm cancel
+      next(true);
+    } else if (!nextRoute.value) {
+      // if there's currently no nextRoute, don't navigate yet and show the confirm cancel dialog
       nextRoute.value = to.path;
       next(false);
+    } else if (!exitConfirmed.value) {
+      // don't go if exit hasn't been confirmed
+      next(false);
     } else {
+      // go if exit confirmed
       next(true);
     }
   });
@@ -42,16 +45,9 @@
     usePipelineRunStore().setParams(JSON.parse(<string>res.params));
   }
 
-  function handleDialogAction() {
+  function confirmCancel() {
     exitConfirmed.value = true;
-    isDialogOpen.value = false;
-    backNavigationInProgress.value = true;
-    $router.push(useUiStore().previousPageRoute);
-    backNavigationInProgress.value = false;
-  }
-
-  function handleExitRun() {
-    isDialogOpen.value = true;
+    $router.push(nextRoute.value!);
   }
 
   /**
@@ -64,19 +60,7 @@
     usePipelineRunStore().setUserPipelineRunName('');
     usePipelineRunStore().setParams({});
     initializePipelineData();
-    resetStepperKey.value++;
   }
-
-  watch([isDialogOpen, backNavigationInProgress], ([dialogOpen, navigatingBack]) => {
-    if (dialogOpen) {
-      nextRoute.value = null;
-      return; // If the dialog is still open, return and don't execute the routing logic
-    }
-    if (!navigatingBack && nextRoute.value && isDialogOpen.value) {
-      $router.push(nextRoute.value);
-      nextRoute.value = null;
-    }
-  });
 </script>
 
 <template>
@@ -84,7 +68,7 @@
     title="Run Pipeline"
     :description="labName"
     :show-back-button="!hasLaunched"
-    :back-action="handleExitRun"
+    :back-action="() => (nextRoute = `/labs/${useLabsStore().labId}?tab=Pipelines`)"
     back-button-label="Exit Run"
   />
   <EGRunPipelineStepper
@@ -92,14 +76,14 @@
     :schema="schema"
     :params="usePipelineRunStore().params"
     @reset-run-pipeline="resetRunPipeline()"
-    :key="resetStepperKey"
   />
   <EGDialog
     action-label="Cancel Pipeline Run"
     :action-variant="ButtonVariantEnum.enum.destructive"
-    @action-triggered="handleDialogAction"
+    @action-triggered="confirmCancel"
     primary-message="Are you sure you would like to cancel?"
     secondary-message="Any changes made or files uploaded will not be saved."
-    v-model="isDialogOpen"
+    :model-value="!!nextRoute"
+    @update:modelValue="nextRoute = null"
   />
 </template>

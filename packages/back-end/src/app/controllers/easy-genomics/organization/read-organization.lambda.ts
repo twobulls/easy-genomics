72e3a4e1398ddad74fc2750058281ad9c89e9b9d
Andrew Purcell <andrew.purcell@deptagency.com>
2024-10-02 15:28:37 +1000

    feat: only access org or lab if user has access

--- packages/back-end/src/app/controllers/easy-genomics/organization/read-organization.lambda.ts
+++ packages/back-end/src/app/controllers/easy-genomics/organization/read-organization.lambda.ts
@@ -1,8 +1,9 @@
 import { Organization } from '@easy-genomics/shared-lib/src/app/types/easy-genomics/organization';
 import { buildErrorResponse, buildResponse } from '@easy-genomics/shared-lib/src/app/utils/common';
-import { RequiredIdNotFoundError } from '@easy-genomics/shared-lib/src/app/utils/HttpError';
+import { RequiredIdNotFoundError, UnauthorizedAccessError } from '@easy-genomics/shared-lib/src/app/utils/HttpError';
 import { APIGatewayProxyResult, APIGatewayProxyWithCognitoAuthorizerEvent, Handler } from 'aws-lambda';
 import { OrganizationService } from '@BE/services/easy-genomics/organization-service';
+import { validateOrganizationAccess, validateSystemAdminAccess } from '@BE/utils/auth-utils';
 
 const organizationService = new OrganizationService();
 
@@ -14,6 +15,11 @@ export const handler: Handler = async (
     // Get Path Parameter
     const id: string = event.pathParameters?.id || '';
     if (id === '') throw new RequiredIdNotFoundError();
+
+    // Only System Admins or Users with access to the Organization can view the organization
+    if (!validateSystemAdminAccess(event) && !validateOrganizationAccess(event, id)) {
+      throw new UnauthorizedAccessError();
+    }
 
     const existing: Organization = await organizationService.get(id);
     return buildResponse(200, JSON.stringify(existing), event);

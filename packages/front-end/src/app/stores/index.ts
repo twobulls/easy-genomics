7b4380a3d2b720c7ab64ab8288ed8c6cabc5ff41
William Holt <william.holt@twobulls.com>
2024-10-11 12:49:45 +1100

    refactor(EG-732): reinstate labs store with cache of labs

--- packages/front-end/src/app/stores/index.ts
+++ packages/front-end/src/app/stores/index.ts
@@ -1,3 +1,4 @@
+import useLabsStore from './labs';
 import useOrgsStore from './orgs';
 import usePipelineRunStore from './pipeline-run';
 import useToastStore from './toast';
@@ -5,9 +6,10 @@ import useUiStore from './ui';
 import useUserStore from './user';
 
 function resetStores() {
+  useLabsStore().reset();
   useOrgsStore().reset();
   usePipelineRunStore().reset();
   useUiStore().reset();
   useUserStore().reset();
 }
-export { useOrgsStore, useToastStore, useUserStore, useUiStore, resetStores, usePipelineRunStore };
+export { resetStores, useOrgsStore, useToastStore, useUserStore, useUiStore, useLabsStore, usePipelineRunStore };
---
+++ packages/front-end/src/app/stores/labs.ts
@@ -0,0 +1,46 @@
+import { Laboratory } from '@easy-genomics/shared-lib/src/app/types/easy-genomics/laboratory';
+import { defineStore } from 'pinia';
+
+interface LabsStoreState {
+  // indexed by orgId then labId
+  labs: Record<string, Laboratory>;
+}
+
+const initialState = (): LabsStoreState => ({
+  labs: {},
+});
+
+const useLabsStore = defineStore('labsStore', {
+  state: initialState,
+
+  actions: {
+    reset() {
+      Object.assign(this, initialState());
+    },
+
+    async loadLabsForOrg(orgId: string): Promise<void> {
+      const { $api } = useNuxtApp();
+      const labs = await $api.labs.list(orgId);
+
+      for (const lab of labs) {
+        this.labs[lab.LaboratoryId] = lab;
+      }
+    },
+
+    async loadAllLabsForCurrentUser(): Promise<void> {
+      const currentUserPermissions = useUserStore().currentUserPermissions.orgPermissions;
+      if (currentUserPermissions === null) {
+        return;
+      }
+
+      const orgIds = Object.keys(currentUserPermissions);
+      await Promise.all(orgIds.map(this.loadLabsForOrg));
+    },
+  },
+
+  getters: {},
+
+  persist: true,
+});
+
+export default useLabsStore;

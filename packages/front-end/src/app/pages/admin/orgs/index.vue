605e7d019e4d3c9c424014165726822b82820435
William Holt <william.holt@twobulls.com>
2024-10-18 12:29:15 +1100

    fix(EG-767): restrict superusers to /admin, block regularusers from /admin

--- packages/front-end/src/app/pages/admin/orgs/index.vue
+++ packages/front-end/src/app/pages/admin/orgs/index.vue
@@ -1,12 +1,3 @@
-<script setup lang="ts">
-  const $router = useRouter();
-
-  // require superuser for admin page
-  if (!useUserStore().isSuperuser) {
-    $router.push('/');
-  }
-</script>
-
 <template>
   <EGAdminAlert />
 

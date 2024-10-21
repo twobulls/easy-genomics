33eca603ee7ceb9d5c03e7ccdf49a1b77554a570
William Holt <william.holt@twobulls.com>
2024-10-21 13:45:48 +1100

    fix(EG-806): user lab roles in org user view erroneously displaying Lab Technician

--- packages/front-end/src/app/types/roles.ts
+++ packages/front-end/src/app/types/roles.ts
@@ -3,5 +3,6 @@ import { z } from 'zod';
 export const LaboratoryRolesEnumSchema = z.nativeEnum({
   LabManager: 'Lab Manager',
   LabTechnician: 'Lab Technician',
+  Unknown: 'Unknown',
 } as const);
 export type LaboratoryRolesEnum = z.infer<typeof LaboratoryRolesEnumSchema>;

60f8d27e51c8e7d97e496f2ab7cb3710cd857f1e
marvinumali <marvin.umali@service.vic.gov.au>
2024-10-23 12:14:23 +1100

    fix(EG-817): disabled used scripts

--- packages/front-end/tests/e2e/sys-admin/EditUserAccess.spec.#e2e.ts
+++ packages/front-end/tests/e2e/sys-admin/EditUserAccess.spec.#e2e.ts
@@ -5,11 +5,11 @@ test("Should change a user's Organization Admin access", async ({ page, baseURL 
   await page.getByRole('link', { name: 'Organizations' }).click();
   await page.getByRole('row', { name: 'Default Organization' }).locator('button').click();
   await page.getByRole('menuitem', { name: 'View / Edit' }).click();
-  await page.getByRole('row', { name: 'Rick Sanchez' }).locator('button').click();
+  await page.getByRole('row', { name: 'Org Admin' }).locator('button').click();
   await page.getByRole('menuitem', { name: 'Edit User Access' }).click();
   await page.waitForLoadState('networkidle');
   await page.locator('span').filter({ hasText: 'Organization Admin' }).click();
   await page.getByRole('status').locator('div').nth(1).click();
   const toastMessage = await page.locator('.test-toast-success').innerText();
-  expect(toastMessage).toContain('Rick Sanchez’s Lab Access has been successfully updated');
+  expect(toastMessage).toContain('Org Admin’s Lab Access has been successfully updated');
 });

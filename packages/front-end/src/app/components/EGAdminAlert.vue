399e8c276f52bb7f84424b2a3dd422a9b9358549
Simon Dib <simon.dib@deptagency.com>
2024-11-07 17:42:27 +1100

    feat: add admin chip

--- packages/front-end/src/app/components/EGAdminAlert.vue
+++ packages/front-end/src/app/components/EGAdminAlert.vue
@@ -1,3 +1,7 @@
 <template>
-  <div style="font-size: 64px; text-align: center">🔑 Admin</div>
+  <div
+    class="relative top-[16px] inline-flex gap-2 rounded-[100px] bg-violet-100 px-4 py-1 text-xs font-semibold uppercase text-violet-700"
+  >
+    System Admin View
+  </div>
 </template>

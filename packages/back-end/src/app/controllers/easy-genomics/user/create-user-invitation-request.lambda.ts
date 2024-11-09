576d2ffdeab4014df46d36c5ceda5f75d9b164c2
Zig Tan <zig.tan@twobulls.com>
2024-11-09 23:43:53 +0800

    fix(EG-533): refactor create-user-invitation-request API to support improved workflow

--- packages/back-end/src/app/controllers/easy-genomics/user/create-user-invitation-request.lambda.ts
+++ packages/back-end/src/app/controllers/easy-genomics/user/create-user-invitation-request.lambda.ts
@@ -1,17 +1,13 @@
 import { createHmac } from 'crypto';
-import {
-  ConditionalCheckFailedException,
-  ResourceNotFoundException,
-  TransactionCanceledException,
-} from '@aws-sdk/client-dynamodb';
+import { ResourceNotFoundException } from '@aws-sdk/client-dynamodb';
+import { OrganizationUserNotFoundError } from '@easy-genomics/shared-lib/lib/app/utils/HttpError';
 import { CreateUserInvitationRequestSchema } from '@easy-genomics/shared-lib/src/app/schema/easy-genomics/user-invitation';
 import { Organization } from '@easy-genomics/shared-lib/src/app/types/easy-genomics/organization';
 import { OrganizationUser } from '@easy-genomics/shared-lib/src/app/types/easy-genomics/organization-user';
 import { User } from '@easy-genomics/shared-lib/src/app/types/easy-genomics/user';
 import { CreateUserInvitationRequest } from '@easy-genomics/shared-lib/src/app/types/easy-genomics/user-invitation';
 import { UserInvitationJwt } from '@easy-genomics/shared-lib/src/app/types/easy-genomics/user-verification-jwt';
-import { buildResponse } from '@easy-genomics/shared-lib/src/app/utils/common';
-import { OrganizationUserNotFoundError } from '@easy-genomics/shared-lib/src/app/utils/HttpError';
+import { buildErrorResponse, buildResponse } from '@easy-genomics/shared-lib/src/app/utils/common';
 import { APIGatewayProxyResult, APIGatewayProxyWithCognitoAuthorizerEvent, Handler } from 'aws-lambda';
 import { CognitoIdpService } from '@BE/services/cognito-idp-service';
 import { OrganizationService } from '@BE/services/easy-genomics/organization-service';
@@ -51,112 +47,109 @@ export const handler: Handler = async (
     const existingUser: User | undefined = (await userService.queryByEmail(request.Email)).shift();
 
     if (!existingUser) {
-      // Attempt to create new Cognito User account - will trigger Cognito process-custom-email-sender to send SES email template
-      const newUserId: string = await cognitoIdpService.adminCreateUser(
-        request.Email,
-        organization.OrganizationId,
-        organization.Name,
-      );
-
-      // Create new User and invite to the Organization and Platform
-      const newUser: User = getNewUser(request.Email, newUserId, currentUserId);
-      const newOrganizationUser: OrganizationUser = getNewOrganizationUser(
-        organization.OrganizationId,
-        newUser.UserId,
-        currentUserId,
-      );
-
-      try {
-        // Attempt to add the new User record, and add the Organization-User access mapping in one transaction
-        if (await platformUserService.addNewUserToOrganization(newUser, newOrganizationUser)) {
-          return buildResponse(200, JSON.stringify({ Status: 'Success' }), event);
-        }
-      } catch (error: unknown) {
-        // TODO - Check to see if we should be returning 200 for errors here
-        console.error(error);
-        return buildResponse(200, JSON.stringify({ Status: 'Error', Message: error.message }), event);
-      }
+      // New User processing logic
+      await inviteNewUserToOrganization(organization, request.Email, currentUserId);
+      return buildResponse(200, JSON.stringify({ Status: 'Success' }), event);
     } else {
-      // Invite existing User to the Organization
-      if (existingUser.Status === 'Inactive') {
-        throw new Error(`Unable to invite User to Organization "${organization.Name}": User Status is "Inactive"`);
-      } else {
-        const existingOrganizationUser: OrganizationUser | void = await organizationUserService
-          .get(organization.OrganizationId, existingUser.UserId)
-          .catch((error: any) => {
-            if (error instanceof ResourceNotFoundException || error instanceof OrganizationUserNotFoundError) {
-              // Do nothing - allow new Organization-User access mapping to proceed.
-            } else {
-              throw error;
-            }
-          });
-
-        // Check if existing Organization-User's Status is still Invited to resend invitation
-        if (existingOrganizationUser) {
-          if (existingOrganizationUser.Status !== 'Invited') {
-            // Existing Organization-User's Status is either Inactive or Active
-            throw new Error(
-              `Unable to re-invite User to Organization "${organization.Name}": User Organization Status is "${existingOrganizationUser.Status}"`,
-            );
-          }
-
-          // Attempt to resend new Cognito User account - will trigger Cognito process-custom-email-sender to send SES email template
-          await cognitoIdpService.adminCreateUser(request.Email, organization.OrganizationId, organization.Name, true); // Resend
+      // Existing User processing logic
+      switch (existingUser.Status) {
+        case 'Invited': // Existing User's Status is still 'Invited' so continue to treat as New User and resend invite
+          await inviteNewUserToOrganization(organization, request.Email, currentUserId, true);
           return buildResponse(200, JSON.stringify({ Status: 'Re-inviting' }), event);
-        } else {
-          // Create new Organization-User access mapping record
-          const newOrganizationUser = getNewOrganizationUser(
-            organization.OrganizationId,
-            existingUser.UserId,
-            currentUserId,
+        case 'Active':
+          await inviteExistingUserToOrganization(organization, existingUser, currentUserId);
+          return buildResponse(200, JSON.stringify({ Status: 'Success' }), event);
+        default:
+          throw new Error(
+            `Unable to invite User to Organization "${organization.Name}": User Status is "${existingUser.Status}"`,
           );
-
-          const existingUserDetails: User = {
-            ...existingUser,
-            OrganizationAccess: {
-              ...existingUser.OrganizationAccess,
-              [organization.OrganizationId]: {
-                Status: newOrganizationUser.Status,
-                LaboratoryAccess: {},
-              },
-            },
-            ModifiedAt: new Date().toISOString(),
-            ModifiedBy: currentUserId,
-          };
-
-          // Attempt to add the User to the Organization in one transaction
-          if (await platformUserService.addExistingUserToOrganization(existingUserDetails, newOrganizationUser)) {
-            await sesService.sendUserInvitationEmail(
-              request.Email,
-              organization.Name,
-              generateExistingUserInvitationJwt(existingUser.Email, existingUser.UserId, organization.OrganizationId),
-            );
-            return buildResponse(200, JSON.stringify({ Status: 'Success' }), event);
-          }
-        }
       }
     }
-  } catch (err: any) {
-    console.error(err);
-    return buildResponse(400, JSON.stringify({ Error: getErrorMessage(err) }), event);
+  } catch (error: any) {
+    console.error(error);
+    return buildErrorResponse(error, event);
   }
 };
 
-/**
- * Helper function to create a new User record.
- * @param email
- * @param userId
- * @param createdBy
- */
-function getNewUser(email: string, userId: string, createdBy?: string): User {
-  const newUser: User = {
-    UserId: userId,
-    Email: email.toLowerCase(),
-    Status: 'Invited',
-    CreatedAt: new Date().toISOString(),
-    CreatedBy: createdBy,
-  };
-  return newUser;
+async function inviteNewUserToOrganization(
+  organization: Organization,
+  email: string,
+  createdBy: string,
+  resend?: boolean,
+) {
+  // Attempt to create new Cognito User account - will trigger Cognito process-custom-email-sender to send SES email template
+  const userId: string = await cognitoIdpService.adminCreateUser(
+    email.toLowerCase(),
+    organization.OrganizationId,
+    organization.Name,
+    resend,
+  );
+
+  if (!resend) {
+    // Attempt to add the new User record, and add the Organization-User access mapping in one transaction
+    await platformUserService.addNewUserToOrganization(
+      {
+        UserId: userId,
+        Email: email.toLowerCase(),
+        Status: 'Invited',
+        CreatedAt: new Date().toISOString(),
+        CreatedBy: createdBy,
+      },
+      {
+        OrganizationId: organization.OrganizationId,
+        UserId: userId,
+        Status: 'Invited',
+        OrganizationAdmin: false,
+        CreatedAt: new Date().toISOString(),
+        CreatedBy: createdBy,
+      },
+    );
+  }
+}
+
+async function inviteExistingUserToOrganization(organization: Organization, user: User, modifiedBy: string) {
+  // Try to find existing OrganizationUser record
+  const existingOrganizationUser: OrganizationUser | void = await organizationUserService
+    .get(organization.OrganizationId, user.UserId)
+    .catch((error: any) => {
+      if (error instanceof ResourceNotFoundException || error instanceof OrganizationUserNotFoundError) {
+        // Do nothing - allow new OrganizationUser access mapping to proceed.
+      } else {
+        throw error;
+      }
+    });
+
+  const organizationUser: OrganizationUser = existingOrganizationUser
+    ? {
+        ...existingOrganizationUser,
+        Status: 'Invited',
+        ModifiedAt: new Date().toISOString(),
+        ModifiedBy: modifiedBy,
+      }
+    : {
+        OrganizationId: organization.OrganizationId,
+        UserId: user.UserId,
+        Status: 'Invited',
+        OrganizationAdmin: false,
+        CreatedAt: new Date().toISOString(),
+        CreatedBy: modifiedBy,
+      };
+
+  await sesService.sendUserInvitationEmail(
+    user.Email,
+    organization.Name,
+    generateExistingUserInvitationJwt(user.Email, user.UserId, organization.OrganizationId),
+  );
+
+  // Attempt to add the new User record, and add the Organization-User access mapping in one transaction
+  await platformUserService.addExistingUserToOrganization(
+    {
+      ...user,
+      ModifiedAt: new Date().toISOString(),
+      ModifiedBy: modifiedBy,
+    },
+    organizationUser,
+  );
 }
 
 /**
@@ -178,33 +171,4 @@ function generateExistingUserInvitationJwt(email: string, userId: string, organi
     CreatedAt: createdAt,
   };
   return generateJwt(existingUserInvitationJwt, process.env.JWT_SECRET_KEY, '7 d');
-}
-
-/**
- * Helper function to create a new Organization-User access mapping record.
- * @param organizationId
- * @param userId
- * @param createdBy
- */
-function getNewOrganizationUser(organizationId, userId: string, createdBy: string): OrganizationUser {
-  const organizationUser: OrganizationUser = {
-    OrganizationId: organizationId,
-    UserId: userId,
-    Status: 'Invited',
-    OrganizationAdmin: false,
-    CreatedAt: new Date().toISOString(),
-    CreatedBy: createdBy,
-  };
-  return organizationUser;
-}
-
-// Used for customising error messages by exception types
-function getErrorMessage(err: any) {
-  if (err instanceof ConditionalCheckFailedException) {
-    return `Create User Invite to Organization failed: ${err.message}`;
-  } else if (err instanceof TransactionCanceledException) {
-    return `Create User Invite to Organization failed: ${err.message}`;
-  } else {
-    return err.message;
-  }
 }

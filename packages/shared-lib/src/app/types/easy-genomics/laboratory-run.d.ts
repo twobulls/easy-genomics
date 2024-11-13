6529272a1070b9c9c1df03e35652f882445d63bb
Zig Tan <zig.tan@twobulls.com>
2024-11-13 11:16:31 +0800

    feat(EG-833): add dynamodb laboratory-run-table (#416)
    * feat(EG-833): update BE to add new DynamoDB laboratory-run-table to track HealthOmics/Seqera Runs
    
    * feat(EG-833): update shared-lib base-entity type def to inc RunType
    
    * feat(EG-833): add shared-lib laboratory-run type def & zod validation schemas

---
+++ packages/shared-lib/src/app/types/easy-genomics/laboratory-run.d.ts
@@ -0,0 +1,41 @@
+/**
+ * The following LaboratoryRun model represents the data stored in the
+ * laboratory-run-table to support tracking Seqera NextFlow / AWS HealthOmics
+ * runs for each Laboratory.
+ *
+ * The LaboratoryId serves as the DynamoDB HashKey, and the RunId
+ * serves as the DynamoDB SortKey - and cannot be modified after creation.
+ *
+ * {
+ *   LaboratoryId: <string>,
+ *   RunId: <string>,
+ *   UserId: <string>,
+ *   OrganizationId: <string>,
+ *   Type: <string>,
+ *   Status: <string>,
+ *   Title?: <string>,
+ *   WorkflowName?: <string>,
+ *   S3Input?: <string>,
+ *   S3Output?: <string>,
+ *   Settings?: <string>, // JSON
+ *   CreatedAt?: <string>,
+ *   CreatedBy?: <string>,
+ *   ModifiedAt?: <string>,
+ *   ModifiedBy?: <string>,
+ * }
+ */
+import { BaseAttributes, RunType } from "../base-entity";
+
+export interface LaboratoryRun extends BaseAttributes {
+  LaboratoryId: string; // DynamoDB Partition Key (String)
+  RunId: string; // DynamoDB Sort Key (String) & Global Secondary Index (String)
+  UserId: string; // Global Secondary Index (String)
+  OrganizationId: string; // Global Secondary Index (String)
+  Type: RunType,
+  Status: string;
+  Title?: string;
+  WorkflowName?: string;
+  S3Input?: string;
+  S3Output?: string;
+  Settings?: string; // JSON
+}

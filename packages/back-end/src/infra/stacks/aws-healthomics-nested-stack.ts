0fa4be3b69d6796d9623b3d270c709e560a1e934
Zig Tan <zig.tan@twobulls.com>
2024-11-28 07:13:31 +0800

    feat(EG-859): update IAM policies to enable HealthOmics Workflow Runs (#432)
    * feat(EG-859): remove unnecessary draft AWS HealthOmics Private Workflow logic & infra code
    
    * feat(EG-859): update iam-construct to add addRole() method
    
    * feat(EG-859): add 'easy-genomics-healthomics-workflow-policy-statements' for role running omics
    
    * feat(EG-859): add 'easy-genomics-healthomics-workflow-run-role' role to start omics workflow runs

--- packages/back-end/src/infra/stacks/aws-healthomics-nested-stack.ts
+++ packages/back-end/src/infra/stacks/aws-healthomics-nested-stack.ts
@@ -1,18 +1,14 @@
 import { NestedStack } from 'aws-cdk-lib';
-import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
-import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { Effect, PolicyDocument, PolicyStatement, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
-import { baseLSIAttributes, DynamoConstruct } from '../constructs/dynamodb-construct';
 import { IamConstruct, IamConstructProps } from '../constructs/iam-construct';
-import { LambdaConstruct } from '../constructs/lambda-construct';
+// import { LambdaConstruct } from '../constructs/lambda-construct';
 import { AwsHealthOmicsNestedStackProps } from '../types/back-end-stack';
 
 export class AwsHealthOmicsNestedStack extends NestedStack {
   readonly props: AwsHealthOmicsNestedStackProps;
-  dynamoDBTables: Map<string, Table> = new Map();
-  dynamoDB: DynamoConstruct;
   iam: IamConstruct;
-  lambda: LambdaConstruct;
+  // lambda: LambdaConstruct;
 
   constructor(scope: Construct, id: string, props: AwsHealthOmicsNestedStackProps) {
     super(scope, id);
@@ -23,122 +19,88 @@ export class AwsHealthOmicsNestedStack extends NestedStack {
     });
     this.setupIamPolicies();
 
-    this.dynamoDB = new DynamoConstruct(this, `${this.props.constructNamespace}-dynamodb`, {
-      devEnv: this.props.devEnv,
-    });
-    this.setupDynamoDBTables();
-
-    this.lambda = new LambdaConstruct(this, `${this.props.constructNamespace}`, {
-      ...this.props,
-      iamPolicyStatements: this.iam.policyStatements, // Pass declared Auth IAM policies for attaching to respective Lambda function
-      lambdaFunctionsDir: 'src/app/controllers/aws-healthomics',
-      lambdaFunctionsNamespace: `${this.props.constructNamespace}`,
-      lambdaFunctionsResources: {}, // Used for setting specific resources for a given Lambda function (e.g. environment settings, trigger events)
-      environment: {
-        // Defines the common environment settings for all lambda functions
-        ACCOUNT_ID: this.props.env.account!,
-        REGION: this.props.env.region!,
-        DOMAIN_NAME: this.props.appDomainName,
-        NAME_PREFIX: this.props.namePrefix,
-      },
-    });
+    // this.lambda = new LambdaConstruct(this, `${this.props.constructNamespace}`, {
+    //   ...this.props,
+    //   iamPolicyStatements: this.iam.policyStatements, // Pass declared Auth IAM policies for attaching to respective Lambda function
+    //   lambdaFunctionsDir: 'src/app/controllers/aws-healthomics',
+    //   lambdaFunctionsNamespace: `${this.props.constructNamespace}`,
+    //   lambdaFunctionsResources: {}, // Used for setting specific resources for a given Lambda function (e.g. environment settings, trigger events)
+    //   environment: {
+    //     // Defines the common environment settings for all lambda functions
+    //     ACCOUNT_ID: this.props.env.account!,
+    //     REGION: this.props.env.region!,
+    //     DOMAIN_NAME: this.props.appDomainName,
+    //     NAME_PREFIX: this.props.namePrefix,
+    //   },
+    // });
   }
 
   // AWS HealthOmics specific IAM policies
   private setupIamPolicies = () => {
-    // /aws-healthomics/private-workflow/create-private-workflow
-    this.iam.addPolicyStatements('/aws-healthomics/private-workflow/create-private-workflow', [
+    /**
+     * To run an Omics workflow, a service role that allows HealthOmics service to access resources is required.
+     * The following policy statements provide the service role permission to:
+     * - All input files locations in S3 will need Read access
+     * - Any S3 output location will require read and write access
+     * - CloudWatch requires access
+     * - Ensure that all ECR containers and buckets required to run the workflow are in the same region
+     */
+    this.iam.addPolicyStatements('easy-genomics-healthomics-workflow-policy-statements', [
+      new PolicyStatement({
+        resources: ['arn:aws:s3:::*/*'],
+        actions: ['s3:*'],
+        effect: Effect.ALLOW,
+      }),
+      new PolicyStatement({
+        resources: ['arn:aws:s3:::*'],
+        actions: ['s3:*'],
+        effect: Effect.ALLOW,
+      }),
       new PolicyStatement({
         resources: [
-          `arn:aws:dynamodb:${this.props.env.region!}:${this.props.env.account!}:table/${this.props.namePrefix}-healthomics-private-workflow-table`,
+          `arn:aws:logs:${this.props.env.region!}:${this.props.env.account!}:log-group:/aws/omics/WorkflowLog:log-stream:*`,
         ],
-        actions: ['dynamodb:PutItem'],
+        actions: ['logs:CreateLogStream', 'logs:DescribeLogStreams', 'logs:PutLogEvents'],
+        effect: Effect.ALLOW,
+      }),
+      new PolicyStatement({
+        resources: [
+          `arn:aws:logs:${this.props.env.region!}:${this.props.env.account!}:log-group:/aws/omics/WorkflowLog:*`,
+        ],
+        actions: ['logs:CreateLogGroup'],
+        effect: Effect.ALLOW,
+      }),
+      new PolicyStatement({
+        resources: [`arn:aws:logs:${this.props.env.region!}:${this.props.env.account!}:repository/*`],
+        actions: ['ecr:BatchCheckLayerAvailability', 'ecr:BatchGetImage', 'ecr:GetDownloadUrlForLayer'],
         effect: Effect.ALLOW,
       }),
     ]);
-    // /aws-healthomics/private-workflow/list-private-workflows
-    this.iam.addPolicyStatements('/aws-healthomics/private-workflow/list-private-workflows', [
-      new PolicyStatement({
-        resources: [
-          `arn:aws:dynamodb:${this.props.env.region!}:${this.props.env.account!}:table/${this.props.namePrefix}-healthomics-private-workflow-table`,
-        ],
-        actions: ['dynamodb:Scan'],
-        effect: Effect.ALLOW,
-      }),
-    ]);
-    // /aws-healthomics/private-workflow/request-private-workflow
-    this.iam.addPolicyStatements('/aws-healthomics/private-workflow/request-private-workflow', [
-      new PolicyStatement({
-        resources: [
-          `arn:aws:dynamodb:${this.props.env.region!}:${this.props.env.account!}:table/${this.props.namePrefix}-healthomics-private-workflow-table`,
-        ],
-        actions: ['dynamodb:GetItem'],
-        effect: Effect.ALLOW,
-      }),
-    ]);
-    // /aws-healthomics/private-workflow/read-private-workflow
-    this.iam.addPolicyStatements('/aws-healthomics/private-workflow/read-private-workflow', [
-      new PolicyStatement({
-        resources: [
-          `arn:aws:dynamodb:${this.props.env.region!}:${this.props.env.account!}:table/${this.props.namePrefix}-healthomics-private-workflow-table`,
-          `arn:aws:dynamodb:${this.props.env.region!}:${this.props.env.account!}:table/${this.props.namePrefix}-healthomics-private-workflow-table/index/*`,
-        ],
-        actions: ['dynamodb:Query'],
-        effect: Effect.ALLOW,
-      }),
-    ]);
-    // /aws-healthomics/private-workflow/update-private-workflow
-    this.iam.addPolicyStatements('/aws-healthomics/private-workflow/update-private-workflow', [
-      new PolicyStatement({
-        resources: [
-          `arn:aws:dynamodb:${this.props.env.region!}:${this.props.env.account!}:table/${this.props.namePrefix}-healthomics-private-workflow-table`,
-          `arn:aws:dynamodb:${this.props.env.region!}:${this.props.env.account!}:table/${this.props.namePrefix}-healthomics-private-workflow-table/index/*`,
-        ],
-        actions: ['dynamodb:Query', 'dynamodb:UpdateItem'],
-        effect: Effect.ALLOW,
-      }),
-    ]);
-    // /aws-healthomics/private-workflow/delete-private-workflow
-    this.iam.addPolicyStatements('/aws-healthomics/private-workflow/delete-private-workflow', [
-      new PolicyStatement({
-        resources: [
-          `arn:aws:dynamodb:${this.props.env.region!}:${this.props.env.account!}:table/${this.props.namePrefix}-healthomics-private-workflow-table`,
-          `arn:aws:dynamodb:${this.props.env.region!}:${this.props.env.account!}:table/${this.props.namePrefix}-healthomics-private-workflow-table/index/*`,
-        ],
-        actions: ['dynamodb:DeleteItem', 'dynamodb:Query'],
-        effect: Effect.ALLOW,
-      }),
-    ]);
-  };
 
-  // AWS HealthOmics specific DynamoDB tables
-  private setupDynamoDBTables = () => {
-    /** Update the definitions below to update / add additional DynamoDB tables **/
-    // AWS HealthOmics Private Workflow table - used for tracking the creation & setup of private workflows to add to AWS HealthOmics
-    const awsHealthOmicsPrivateWorkflowsTableName = `${this.props.namePrefix}-healthomics-private-workflow-table`;
-    const awsHealthOmicsPrivateWorkflowsTable = this.dynamoDB.createTable(
-      awsHealthOmicsPrivateWorkflowsTableName,
-      {
-        partitionKey: {
-          name: 'Url', // Workflow Repository URL
-          type: AttributeType.STRING,
-        },
-        sortKey: {
-          name: 'Version', // Workflow Release Version
-          type: AttributeType.STRING,
-        },
-        gsi: [
-          {
-            partitionKey: {
-              name: 'PrivateWorkflowId', // Global Secondary Index to support REST API get / update / delete requests
-              type: AttributeType.STRING,
+    /**
+     * To start an Omics workflow run, a service role with the appropriate access policies is required.
+     */
+    this.iam.addRole(
+      'easy-genomics-healthomics-workflow-run-role',
+      new Role(this, `${this.props.namePrefix}-easy-genomics-healthomics-workflow-run-role`, {
+        roleName: `${this.props.namePrefix}-easy-genomics-healthomics-workflow-run-role`,
+        assumedBy: new ServicePrincipal('omics.amazonaws.com', {
+          region: `${this.props.env.region!}`,
+          conditions: {
+            ['StringEquals']: {
+              'aws:SourceAccount': `${this.props.env.account!}`,
+            },
+            ['ArnLike']: {
+              'aws:SourceArn': `arn:aws:omics:${this.props.env.region}:${this.props.env.account!}:run/*`,
             },
           },
-        ],
-        lsi: baseLSIAttributes,
-      },
-      this.props.devEnv,
+        }),
+        inlinePolicies: {
+          [`${this.props.namePrefix}-easy-genomics-healthomics-workflow-policy-document`]: new PolicyDocument({
+            statements: this.iam.getPolicyStatements('easy-genomics-healthomics-workflow-policy-statements'),
+          }),
+        },
+      }),
     );
-    this.dynamoDBTables.set(awsHealthOmicsPrivateWorkflowsTableName, awsHealthOmicsPrivateWorkflowsTable);
   };
 }

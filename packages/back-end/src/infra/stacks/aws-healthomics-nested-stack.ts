42dbcdc0faa2ae871f69a05b208eb842dd8e9ce7
Zig Tan <zig.tan@twobulls.com>
2024-12-03 12:09:54 +0800

    feat(EG-875): update BE infra to add IAM policies to support 'create-run-execution' API

--- packages/back-end/src/infra/stacks/aws-healthomics-nested-stack.ts
+++ packages/back-end/src/infra/stacks/aws-healthomics-nested-stack.ts
@@ -180,5 +180,20 @@ export class AwsHealthOmicsNestedStack extends NestedStack {
         effect: Effect.ALLOW,
       }),
     ]);
+    // /aws-healthomics/run/create-run-execution
+    this.iam.addPolicyStatements('/aws-healthomics/run/create-run-execution', [
+      new PolicyStatement({
+        resources: [
+          `arn:aws:dynamodb:${this.props.env.region!}:${this.props.env.account!}:table/${this.props.namePrefix}-laboratory-table`,
+          `arn:aws:dynamodb:${this.props.env.region!}:${this.props.env.account!}:table/${this.props.namePrefix}-laboratory-table/index/*`,
+        ],
+        actions: ['dynamodb:Query'],
+      }),
+      new PolicyStatement({
+        resources: [`arn:aws:omics:${this.props.env.region!}:${this.props.env.account!}:run/*`],
+        actions: ['omics:StartRun'],
+        effect: Effect.ALLOW,
+      }),
+    ]);
   };
 }

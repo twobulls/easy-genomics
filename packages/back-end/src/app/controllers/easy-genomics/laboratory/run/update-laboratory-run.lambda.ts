eac62a89df1463bf1d56f20758422518e2e1a9be
Andrew Purcell <115062811+PurcyTwoBulls@users.noreply.github.com>
2024-12-17 13:00:52 +1100

    fix(EG-881): laboratory run apis error on missing run (#458)
    * fix: throw error if laboratory run not found
    
    * fix: do not allow runid or laboratoryid to be updated
    
    ---------
    
    Co-authored-by: Zig Tan <zig.tan@twobulls.com>

--- packages/back-end/src/app/controllers/easy-genomics/laboratory/run/update-laboratory-run.lambda.ts
+++ packages/back-end/src/app/controllers/easy-genomics/laboratory/run/update-laboratory-run.lambda.ts
@@ -6,6 +6,7 @@ import { LaboratoryRun } from '@easy-genomics/shared-lib/src/app/types/easy-geno
 import { buildErrorResponse, buildResponse } from '@easy-genomics/shared-lib/src/app/utils/common';
 import {
   InvalidRequestError,
+  LaboratoryRunNotFoundError,
   RequiredIdNotFoundError,
   UnauthorizedAccessError,
 } from '@easy-genomics/shared-lib/src/app/utils/HttpError';
@@ -40,6 +41,10 @@ export const handler: Handler = async (
 
     // Lookup by RunId to confirm existence before updating
     const existing: LaboratoryRun = await laboratoryRunService.queryByRunId(id);
+
+    if (!existing) {
+      throw new LaboratoryRunNotFoundError(id);
+    }
 
     // Only available for Org Admins or Laboratory Managers and Technicians
     if (

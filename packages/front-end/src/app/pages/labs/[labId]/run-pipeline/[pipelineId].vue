eab462206f6927bb0689cbfa037181465958be67
Zig Tan <zig.tan@twobulls.com>
2025-01-10 08:32:53 +0800

    fix(EG-921): remove unnecessary pipelineName from WipSeqeraRunData store

--- packages/front-end/src/app/pages/labs/[labId]/run-pipeline/[pipelineId].vue
+++ packages/front-end/src/app/pages/labs/[labId]/run-pipeline/[pipelineId].vue
@@ -2,10 +2,7 @@
   import { useRunStore } from '@FE/stores';
   import { ButtonVariantEnum } from '@FE/types/buttons';
   import { v4 as uuidv4 } from 'uuid';
-  import {
-    DescribePipelineSchemaResponse,
-    DescribePipelinesResponse,
-  } from '@/packages/shared-lib/src/app/types/nf-tower/nextflow-tower-api';
+  import { DescribePipelineSchemaResponse } from '@/packages/shared-lib/src/app/types/nf-tower/nextflow-tower-api';
 
   const { $api } = useNuxtApp();
   const $router = useRouter();
@@ -63,11 +60,9 @@
    * Reads the pipeline details, schema, and parameters from the API and initializes the pipeline run store
    */
   async function initializePipelineData() {
-    const pipelineResponse: DescribePipelinesResponse = await $api.seqeraPipelines.read(pipelineId, labId);
     runStore.updateWipSeqeraRun(seqeraRunTempId.value, {
       laboratoryId: labId,
       pipelineId: pipelineId,
-      pipelineName: pipelineResponse.pipeline?.name,
       transactionId: seqeraRunTempId.value,
     });
 

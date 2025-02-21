b3c98c768c6d122cbb51f794b5523f8789ccecce
William Holt <william.holt@twobulls.com>
2025-02-21 15:47:31 +1100

    fix(EG-851): no labs screen showing two create a lab buttons

--- packages/front-end/src/app/pages/labs/index.vue
+++ packages/front-end/src/app/pages/labs/index.vue
@@ -1,16 +1,19 @@
 <script setup lang="ts">
   const $router = useRouter();
+
+  const userStore = useUserStore();
+  const labsStore = useLabsStore();
 </script>
 
 <template>
   <EGPageHeader title="Labs" :show-back="false">
     <EGButton
-      v-if="useUserStore().canCreateLab()"
+      v-if="userStore.canCreateLab() && labsStore.labsForOrg(userStore.currentOrgId!).length > 0"
       label="Create a new Lab"
       class="self-end"
       @click="() => $router.push({ path: `/labs/create` })"
     />
   </EGPageHeader>
 
-  <EGLabsList :org-id="useUserStore().currentOrgId" />
+  <EGLabsList :org-id="userStore.currentOrgId" />
 </template>

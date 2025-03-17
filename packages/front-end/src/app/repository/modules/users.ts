41b6fb08fa5ce5cb02003d0ecf1215884da26c8d
Zig Tan <zig.tan@twobulls.com>
2025-03-17 09:31:59 +0800

    feat(EG-1052): update FE UsersModule to support updateUserLastAccessInfo() API requests

--- packages/front-end/src/app/repository/modules/users.ts
+++ packages/front-end/src/app/repository/modules/users.ts
@@ -1,5 +1,7 @@
 import {
   UpdateUserDefaultOrganizationSchema,
+  UpdateUserLastAccessedInfo,
+  UpdateUserLastAccessedInfoSchema,
   UpdateUserSchema,
 } from '@easy-genomics/shared-lib/src/app/schema/easy-genomics/user';
 import {
@@ -160,6 +162,27 @@ class UsersModule extends HttpFactory {
 
     if (!res) {
       throw new Error('Error updating user default organization');
+    }
+
+    return res;
+  }
+
+  async updateUserLastAccessInfo(userId: string, organizationId?: string, laboratoryId?: string) {
+    const data: UpdateUserLastAccessedInfo = {
+      OrganizationId: organizationId,
+      LaboratoryId: laboratoryId,
+    };
+    const parseResult = UpdateUserLastAccessedInfoSchema.safeParse(data);
+    if (!parseResult.success) {
+      console.error('Error; updateUserLastAccessInfo; safe parse failed; parseResult: ', parseResult);
+      throw new Error(
+        `Error; updateUserLastAccessInfo; safe parse failed; parseResult: ${JSON.stringify(parseResult, null, 2)}`,
+      );
+    }
+
+    const res = await this.call<{}>('PUT', `/user/update-user-last-accessed-info/${userId}`, data);
+    if (!res) {
+      throw new Error('Error updating user last accessed info');
     }
 
     return res;

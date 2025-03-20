76bc22a4e1237ef698479a76f08b71ddd7b1853f
Zig Tan <zig.tan@twobulls.com>
2025-03-20 11:29:39 +0800

    feat(EG-1077): simplify samplesheet name to only inc Run Name and remove regex support for [] chars

--- packages/front-end/src/app/components/EGRunFormUploadData.vue
+++ packages/front-end/src/app/components/EGRunFormUploadData.vue
@@ -497,10 +497,9 @@
   async function getSampleSheetCsv(uploadedFilePairs: UploadedFilePairInfo[]): Promise<SampleSheetResponse> {
     if (!wipRun.value.transactionId) throw new Error('no transaction id on wip run');
 
-    const sampleSheetName = `samplesheet-${props.pipelineOrWorkflowName}-[${wipRun.value.runName}].csv`.replace(
-      '/',
-      ':',
-    );
+    const sampleSheetName: string = wipRun.value.runName
+      ? `samplesheet-${wipRun.value.runName}.csv`
+      : 'samplesheet.csv';
 
     const request: SampleSheetRequest = {
       SampleSheetName: sampleSheetName,
